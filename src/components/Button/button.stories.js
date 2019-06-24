a8341a047f0242e02c66fb38b8b28513baeec5ed
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-06-24 18:17:45 +0200

    Add primary button styles

--- src/components/Button/button.stories.js
+++ src/components/Button/button.stories.js
@@ -5,6 +5,6 @@ import { action } from '@storybook/addon-actions';
 
 import Button from './index';
 
-storiesOf('Button', module).add('with text', () => (
-  <Button onClick={action('clicked')}>Hello Button</Button>
+storiesOf('Button', module).add('primary', () => (
+  <Button onClick={action('clicked')}>Earn Monero</Button>
 ));
