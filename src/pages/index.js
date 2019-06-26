ca6c3d4f4bf97993b811701554ee45dd089fb3dd
Maciej Sykuła <maciek.sykula@gmail.com>
2019-06-26 10:11:03 +0200

    Import DateCounter to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Navbar from '../components/Navbar';
+import DateCounter from '../components/DateCounter';
 
 const IndexPage = () => (
   <div>
     <Navbar />
+    <DateCounter />
   </div>
 );
 
