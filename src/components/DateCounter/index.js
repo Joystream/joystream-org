2bd27c1292200c2df4fffb69cb18ba6f9cd6679b
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-06-27 18:05:03 +0200

    DateCounter fix

--- src/components/DateCounter/index.js
+++ src/components/DateCounter/index.js
@@ -20,17 +20,20 @@ const defaultProps = {
   light: false,
 };
 
-const renderer = ({ total, days, hours, minutes, seconds }, { date, light, large }) => {
+const renderer = (
+  { total, days, hours, minutes, seconds },
+  { date, light, large }
+) => {
   let defaultTitle = 'Time to launch';
   let defaultSeparator = ':';
   let timeToDisplay = [];
 
-  if(!total) {
+  if (!total) {
     defaultTitle = 'Launched on';
     defaultSeparator = '/';
     timeToDisplay = [
       { label: 'day', value: new Date(date).getUTCDate() },
-      { label: 'month', value: new Date(date).getUTCMonth() },
+      { label: 'month', value: new Date(date).getUTCMonth() + 1 },
       { label: 'year', value: new Date(date).getUTCFullYear() },
     ];
   } else {
@@ -55,23 +58,31 @@ const renderer = ({ total, days, hours, minutes, seconds }, { date, light, large
   }
 
   return (
-    <div className={ cn('DateCounter', { 'DateCounter--light': light, 'DateCounter--large': large }) }>
-      <p className="DateCounter__title">{ defaultTitle }</p>
+    <div
+      className={cn('DateCounter', {
+        'DateCounter--light': light,
+        'DateCounter--large': large,
+      })}
+    >
+      <p className="DateCounter__title">{defaultTitle}</p>
       <div className="DateCounter__container">
-        { timeToDisplay.map((time, i) => {
+        {timeToDisplay.map((time, i) => {
           return (
-            <React.Fragment key={ time.label }>
+            <React.Fragment key={time.label}>
               <div className="DateCounter__segment">
-                <span className="DateCounter__value">{ padNumber(time.value) }</span>
-                <span className="DateCounter__label">{ time.label }</span>
+                <span className="DateCounter__value">
+                  {padNumber(time.value)}
+                </span>
+                <span className="DateCounter__label">{time.label}</span>
               </div>
-              { i + 1 !== timeToDisplay.length &&
+              {i + 1 !== timeToDisplay.length && (
                 <span className="DateCounter__value, DateCounter__separator">
-                  { defaultSeparator }
-                </span> }
+                  {defaultSeparator}
+                </span>
+              )}
             </React.Fragment>
           );
-        }) }
+        })}
       </div>
     </div>
   );
@@ -80,8 +91,9 @@ const renderer = ({ total, days, hours, minutes, seconds }, { date, light, large
 const DateCounter = ({ date, ...props }) => {
   return (
     <Countdown
-      date={ date }
-      renderer={ (countdownProps) => renderer(countdownProps, { date, ...props }) } />
+      date={date}
+      renderer={countdownProps => renderer(countdownProps, { date, ...props })}
+    />
   );
 };
 
