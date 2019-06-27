e40847abcad730ea84cce3a65b7d66b8d529be2d
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-06-27 18:06:03 +0200

    Add error and announced version of Hero Card Component

--- src/components/HeroCard/HeroCard.stories.js
+++ src/components/HeroCard/HeroCard.stories.js
@@ -6,6 +6,9 @@ import HeroCard from './';
 
 const today = new Date();
 const unfinishedDate = new Date();
-unfinishedDate.setMinutes(today.getMinutes() + 5);
+unfinishedDate.setMinutes(today.getMinutes() + 1);
 
-storiesOf('HeroCard', module).add('In progress', () => <HeroCard />);
+storiesOf('HeroCard', module)
+  .add('HeroCard in progress', () => <HeroCard date={unfinishedDate} />)
+  .add('HeroCard error', () => <HeroCard date={null} />)
+  .add('HeroCard done', () => <HeroCard date="2019/06/27 17:50" />);
--- src/components/HeroCard/index.js
+++ src/components/HeroCard/index.js
@@ -1,32 +1,60 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { instanceOf, oneOfType, string } from 'prop-types';
 import cn from 'classnames';
 
 import './style.scss';
 import { ReactComponent as Calendar } from '../../assets/svg/calendar.svg';
+import { ReactComponent as Warning } from '../../assets/svg/warning.svg';
 import DateCounter from '../DateCounter';
 
 const propTypes = {
-  title: PropTypes.string.isRequired,
+  title: string,
+  date: oneOfType([string, instanceOf(Date)]),
 };
 
 const defaultProps = {
   title: 'Network live',
+  date: null,
 };
 
-const HeroCard = ({ title, error, ...props }) => {
+const HeroCard = ({ date, title, error, ...props }) => {
   const classes = cn('Card', {
-    'Card--error': error,
+    'Card--error': !date,
   });
+
+  const now = new Date();
+  const dateObj = new Date(date);
 
   return (
     <section className={classes} {...props}>
       <h2 className="Card__header">
-        <Calendar className="Card__icon" />
-        {title}
+        {!date ? (
+          <>
+            <Warning className="Card__icon" />
+            Network down
+          </>
+        ) : (
+          <>
+            <Calendar className="Card__icon" />
+            {now > dateObj ? 'Network announced' : 'Network live'}
+          </>
+        )}
       </h2>
       <div className="Card__content">
-        <DateCounter date="2019/06/25 16:26" large light />
+        {!date ? (
+          <div>
+            <p className="Card__title"> Failure identified</p>
+            <p className="Card__text">
+              {' '}
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, ad
+              cum, doloremque dolores dolorum ducimus est facere facilis illo in
+              nam neque non optio quaerat quisquam repellendus velit, vero
+              voluptatem?
+            </p>
+          </div>
+        ) : (
+          <DateCounter date={date} large light />
+        )}
       </div>
     </section>
   );
