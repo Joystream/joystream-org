051d489b0ddae16055f3d9230e9b0d0f51a67b8a
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-06-27 14:16:24 +0200

    Update svg loader config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -29,5 +29,26 @@ module.exports = ({ config }) => {
     include: path.resolve(__dirname, '../'),
   });
 
-  return config
-}
+  config.module.rules = config.module.rules.map(rule => {
+    if (
+      String(rule.test) ===
+      String(
+        /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/
+      )
+    ) {
+      return {
+        ...rule,
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
+      };
+    }
+
+    return rule;
+  });
+
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack', 'url-loader'],
+  });
+
+  return config;
+};
