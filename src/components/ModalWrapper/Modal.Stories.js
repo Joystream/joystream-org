6d37b57f05ccef7db0fda0f646e6e6d0fa2c6ea2
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-07-02 15:50:17 +0200

    ModalWrapper Component

--- src/components/ModalWrapper/Modal.Stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import ModalWrapper from './';
-
-storiesOf('ModalWrapper ', module).add('default', () => <ModalWrapper />);
