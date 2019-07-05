0832bb100b433776c7b58b6954f2baddfb2e2225
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-05 10:30:10 +0200

    Add Hero section with example custom storied

---
+++ src/components/Hero/hero.stories.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { ReactComponent as MasksImage } from '../../assets/svg/masks.svg';
+import { ReactComponent as PlatformImage } from '../../assets/svg/platform.svg';
+import { ReactComponent as AcropolisImage } from '../../assets/svg/acropolis.svg';
+
+import Button from '../Button';
+import Link from '../Link';
+import HeroCard from '../HeroCard';
+
+import Hero from './';
+import './hero.stories.scss';
+
+const today = new Date();
+const unfinishedDate = new Date();
+unfinishedDate.setHours(today.getHours() + 2);
+
+storiesOf('Hero', module)
+  .addParameters({
+    backgrounds: [{ name: 'black', value: '#000000', default: true }],
+  })
+  .add('plain content with an image', () => (
+    <Hero image={MasksImage} title="Discover various roles on the platform">
+      <p className="HeroStory__paragraph">
+        Explore available roles and pick the one that suits you the most. Influence platforms development earning Monero
+        in the process.
+      </p>
+    </Hero>
+  ))
+  .add('custon content', () => (
+    <Hero image={PlatformImage} title="A user governed video platform">
+      <p className="HeroStory__paragraph">Earn Monero by participating in the current Athens testnet</p>
+      <div className="HeroStory__group">
+        <Button secondary className="HeroStory__button">
+          Earn Monero
+        </Button>
+        <Button secondary reversed className="HeroStory__button">
+          Launch UI
+        </Button>
+      </div>
+    </Hero>
+  ))
+  .add('without image', () => (
+    <Hero title="Discover various roles on the platform">
+      <p className="HeroStory__paragraph">
+        <strong>Jsgenesis values your privacy.</strong>
+        This Privacy Policy ("Privacy Policy") and Cookie Policy
+        ("Cookie Policy") explains how Jsgenesis AS ("Jsgenesis",
+        "Company", "We", "Us", "Our") collect and use data and information
+        when you ("User) use on or any of the Joystream products, developed in the GitHub organization
+        <Link href="https://github.com/JoyStream"  highlighted> Joystream</Link>.
+        These products (collectively "Software") include, but are not limited to, all pages under the
+        <Link href="https://www.joystream.org/" highlighted> joystream.org </Link>
+        domain ("Website"), the
+        <Link href="https://github.com/Joystream/substrate-node-joystream" highlighted> Joyful node </Link>
+        ("Full Node"), the
+        <Link href="https://github.com/Joystream/storage-node-joystream" highlighted> Colossus Storage Node </Link>
+        ("Storage node"), and the Pioneer User Interface, either
+        <Link href="https://github.com/Joystream/apps" highlighted> self hosted </Link>
+        or
+        <Link href="http://testnet.joystream.org/" highlighted> hosted by Us </Link>
+        ("App").
+      </p>
+    </Hero>
+  ))
+  .add('with indent and counter', () => (
+    <Hero title="Acropolis Network" image={ AcropolisImage } indent>
+      <p className="HeroStory__paragraph">
+        Explore available roles and pick the one that suits you the most.
+        Influence platforms development earning Monero in the process.
+      </p>
+      <HeroCard date={ unfinishedDate }/>
+    </Hero>
+  ), {
+    backgrounds: [{ name: 'white', value: '#ffffff', default: true }],
+  })
+  ;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import cn from 'classnames';
+import { string, func, bool, oneOfType } from 'prop-types';
+import Plx from 'react-plx';
+
+import './style.scss';
+
+const propTypes = {
+  title: string.isRequired,
+  image: oneOfType([func, string]),
+  indent: bool,
+};
+
+const defaultProps = {
+  image: null,
+  indent: false,
+};
+
+const Hero = ({ title, children, image: Image, indent, ...props }) => {
+  const parallaxData = [
+    {
+      start: 0,
+      end: 500,
+      properties: [
+        {
+          startValue: 0,
+          endValue: -100,
+          property: 'translateY',
+        },
+      ],
+    },
+  ];
+
+  return (
+    <section className={ cn('Hero', { 'Hero--indented': indent })}>
+      <div className="Hero__container">
+        <div className="Hero__content">
+          <h1 className="Hero__title">{title}</h1>
+          <div className="Hero__description">{children}</div>
+        </div>
+
+        {Image && <Plx
+          parallaxData={ parallaxData }
+        >
+          <Image className="Hero__image" />
+        </Plx>}
+      </div>
+    </section>
+  );
+};
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
