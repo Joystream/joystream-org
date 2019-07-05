ffc2c5e88004e803709ea51ea29be4c4a8bed92a
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-05 10:30:57 +0200

    Add Hero example to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { string, shape, number } from 'prop-types';
 
 import getApiPath from '../utils/getApiPath';
 import mapStatusDataToAnalytics from '../utils/mapStatusDataToAnalytics';
+import { ReactComponent as AcropolisImage } from '../assets/svg/acropolis.svg';
 
 import withApi from '../components/_enhancers/withApi';
 
@@ -12,6 +13,9 @@ import DateCounter from '../components/DateCounter';
 import Analytics from '../components/Analytics';
 import Button from '../components/Button';
 import TitleWrapper from '../components/TitleWrapper';
+import Hero from '../components/Hero';
+
+import './style.scss';
 
 const propTypes = {
   content: shape({
@@ -51,9 +55,20 @@ const propTypes = {
   }).isRequired,
 };
 
+const today = new Date();
+const unfinishedDate = new Date();
+unfinishedDate.setHours(today.getHours() + 2);
+
 const IndexPage = ({ content }) => (
   <div>
     <Navbar />
+    <Hero title="Acropolis Network" image={ AcropolisImage } indent>
+      <p className="HeroStory__paragraph">
+        Explore available roles and pick the one that suits you the most.
+        Influence platforms development earning Monero in the process.
+      </p>
+      <HeroCard date={ unfinishedDate }/>
+    </Hero>
     <HeroCard date="2019/06/27 17:50" />
     <DateCounter date="2019/06/27 17:50" />
     <Analytics content={mapStatusDataToAnalytics(content)}>
