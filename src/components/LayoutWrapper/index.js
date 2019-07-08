44a772e46d224c72d92649db9c58c039cf5930ca
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-08 15:25:09 +0200

    Add LayoutWrapper component

---
+++ src/components/LayoutWrapper/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import cn from 'classnames';
+import { node, string, bool } from 'prop-types';
+
+import './style.scss';
+
+const propTypes = {
+  children: node.isRequired,
+  dark: bool,
+  className: string,
+};
+
+const defaultProps = {
+  className: '',
+  dark: false,
+};
+
+const LayoutWrapper = ({ children, dark, className, ...props }) => {
+  const classes = cn('LayoutWrapper', {
+    'LayoutWrapper--dark': dark,
+  });
+
+  return (
+    <div className={classes} {...props}>
+      <div className="LayoutWrapper__container">{children}</div>
+    </div>
+  );
+};
+
+LayoutWrapper.propTypes = propTypes;
+LayoutWrapper.defaultProps = defaultProps;
+
+export default LayoutWrapper;
