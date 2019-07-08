7636ace4f4baccabc78f2357f486112dd2677c10
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-07-08 14:14:14 +0200

    Add State to Storybook, refactor of TestnetModal

--- src/components/TestnetModal/TestnetModal.stories.js
+++ src/components/TestnetModal/TestnetModal.stories.js
@@ -1,62 +1,90 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { State, Store } from '@sambego/storybook-state';
 
-import ModalWrapper from '../ModalWrapper/';
 import TestnetModal from './index';
+import Button from '../Button';
 import AcropolisImg from '../../assets/svg/acropolis.svg';
 import HelmetImg from '../../assets/svg/helmet.svg';
 import OwlImg from '../../assets/svg/athens-owl.svg';
-import { action } from '@storybook/addon-actions';
+
+const acropolisStore = new Store({
+  isOpen: false,
+});
+const athensStore = new Store({
+  isOpen: false,
+});
+const spartaStore = new Store({
+  isOpen: false,
+});
 
 storiesOf('TestnetModal', module)
   .add('Acropolis', () => (
-    <ModalWrapper isOpen={true}>
-      <TestnetModal
-        title="The Acropolis of Athens"
-        image={AcropolisImg}
-        closeModal={action('close modal')}
-      >
-        <p className="TestnetModal__paragraph">
-          <strong>Known for its great architecture, Acropolis'</strong> perhaps
-          most famous building is the Parthenon. It was built to celebrate their
-          victory over Persian invaders, and is today seen as a symbol for
-          democracy and western civilization.
-        </p>
-      </TestnetModal>
-    </ModalWrapper>
+    <div>
+      <State store={acropolisStore}>
+        <TestnetModal
+          title="The Acropolis of Athens"
+          image={AcropolisImg}
+          closeModal={() => acropolisStore.set({ isOpen: false })}
+          isOpen
+        >
+          <p>
+            <strong>Known for its great architecture, Acropolis'</strong>{' '}
+            perhaps most famous building is the Parthenon. It was built to
+            celebrate their victory over Persian invaders, and is today seen as
+            a symbol for democracy and western civilization.
+          </p>
+        </TestnetModal>
+      </State>
+      <Button onClick={() => acropolisStore.set({ isOpen: true })}>
+        Show modal
+      </Button>
+    </div>
   ))
   .add('Sparta', () => (
-    <ModalWrapper isOpen={true}>
-      <TestnetModal
-        title="Helmet of Sparta"
-        image={HelmetImg}
-        closeModal={action('close modal')}
-      >
-        <p className="TestnetModal__paragraph">
-          <strong>The Spartan army helmet</strong> represents what Sparta is
-          most known for today, their military might. For it's time, it was also
-          known for its advanced governance system. The helmet is usually
-          depicted with the distinctive horsehair crest, although it was
-          possibly only officers that were given them.
-        </p>
-      </TestnetModal>
-    </ModalWrapper>
+    <div>
+      <State store={athensStore}>
+        <TestnetModal
+          title="Helmet of Sparta"
+          image={HelmetImg}
+          closeModal={() => athensStore.set({ isOpen: false })}
+          isOpen
+        >
+          <p>
+            <strong>The Spartan army helmet</strong> represents what Sparta is
+            most known for today, their military might. For it's time, it was
+            also known for its advanced governance system. The helmet is usually
+            depicted with the distinctive horsehair crest, although it was
+            possibly only officers that were given them.
+          </p>
+        </TestnetModal>
+      </State>
+      <Button onClick={() => athensStore.set({ isOpen: true })}>
+        Show modal
+      </Button>
+    </div>
   ))
   .add('Athens', () => (
-    <ModalWrapper isOpen={true}>
-      <TestnetModal
-        title="Owl of Athena"
-        image={OwlImg}
-        closeModal={action('close modal')}
-      >
-        <p className="TestnetModal__paragraph">
-          <strong>In Greek mythology, a little owl (Athene noctua)</strong>{' '}
-          traditionally represents or accompanies Athena, the virgin goddess of
-          wisdom. Because of such association, the bird — often referred to as
-          the "owl of Athena" — has been used as a symbol of knowledge, wisdom,
-          perspicacity and erudition throughout the Western world.
-        </p>
-      </TestnetModal>
-    </ModalWrapper>
+    <div>
+      <State store={spartaStore}>
+        <TestnetModal
+          title="Owl of Athena"
+          image={OwlImg}
+          closeModal={() => spartaStore.set({ isOpen: false })}
+          isOpen
+        >
+          <p>
+            <strong>In Greek mythology, a little owl (Athene noctua)</strong>{' '}
+            traditionally represents or accompanies Athena, the virgin goddess
+            of wisdom. Because of such association, the bird — often referred to
+            as the "owl of Athena" — has been used as a symbol of knowledge,
+            wisdom, perspicacity and erudition throughout the Western world.
+          </p>
+        </TestnetModal>
+      </State>
+      <Button onClick={() => spartaStore.set({ isOpen: true })}>
+        Show modal
+      </Button>
+    </div>
   ));
--- src/components/TestnetModal/index.js
+++ src/components/TestnetModal/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { string, node, func } from 'prop-types';
+import { string, node, func, bool } from 'prop-types';
 import cn from 'classnames';
 import Button from '../Button';
+import ModalWrapper from '../ModalWrapper';
 
 import './style.scss';
 
 const propTypes = {
   title: string.isRequired,
   children: node.isRequired,
-  className: string,
   image: func,
+  closeModal: func.isRequired,
+  isOpen: bool.isRequired,
+  className: string,
 };
 
 const defaultProps = {
@@ -17,24 +20,33 @@ const defaultProps = {
   image: null,
 };
 
-const TestnetModal = ({ title, className, children, image, closeModal }) => {
+const TestnetModal = ({
+  title,
+  children,
+  image,
+  closeModal,
+  className,
+  isOpen,
+}) => {
   const classes = cn(className, 'TestnetModal');
 
   return (
-    <section className={classes}>
-      {image && (
-        <div className="TestnetModal__header">
-          <img className="TestnetModal__image" src={image} alt={title} />
+    <ModalWrapper isOpen={isOpen}>
+      <div className={classes}>
+        {image && (
+          <div className="TestnetModal__header">
+            <img className="TestnetModal__image" src={image} alt={title} />
+          </div>
+        )}
+        <div className="TestnetModal__container">
+          <h2 className="TestnetModal__title">{title}</h2>
+          {children}
+          <Button onClick={closeModal} large className="TestnetModal__button">
+            Close
+          </Button>
         </div>
-      )}
-      <div className="TestnetModal__container">
-        <h2 className="TestnetModal__title">{title}</h2>
-        {children}
-        <Button onClick={closeModal} large className="TestnetModal__button">
-          Close
-        </Button>
       </div>
-    </section>
+    </ModalWrapper>
   );
 };
 
