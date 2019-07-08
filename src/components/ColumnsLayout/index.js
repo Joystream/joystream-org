797e6391dae0795fde0996fe2d8c78034263f9a6
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-08 15:25:41 +0200

    Add ColumnsLayout component

---
+++ src/components/ColumnsLayout/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { node } from 'prop-types';
+
+import './style.scss';
+
+const propTypes = {
+  columnsCount: (props, propName, componentName) => {
+    if (props[propName] < 1 || props[propName] > 4) {
+      return new Error(`Prop '${propName}' specified in '${componentName}' should be a number between 1 and 4.`);
+    }
+    if (typeof props[propName] !== 'number') {
+      return new Error(`Prop '${propName}' specified in '${componentName}' should be a number.`);
+    }
+  },
+  children: node.isRequired,
+};
+
+const defaultProps = {
+  columnsCount: 2,
+};
+
+const ColumnsLayout = ({ columnsCount, children }) => {
+  return (
+    <div className="ColumnsLayout" data-columns={columnsCount}>
+      {children}
+    </div>
+  );
+};
+
+ColumnsLayout.propTypes = propTypes;
+ColumnsLayout.defaultProps = defaultProps;
+
+export default ColumnsLayout;
