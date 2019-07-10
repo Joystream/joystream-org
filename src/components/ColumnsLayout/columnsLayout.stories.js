0cefc7bb011829244bff78409c0f6ec63ea0d556
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-10 10:50:33 +0200

    Update after review

--- src/components/ColumnsLayout/columnsLayout.stories.js
+++ src/components/ColumnsLayout/columnsLayout.stories.js
@@ -3,13 +3,12 @@ import { storiesOf } from '@storybook/react';
 
 import ColumnsLayout from './index';
 
-const randomBgColor = () => (
+const randomBgColor = () =>
   `hsl(
     ${360 * Math.random()},
     ${25 + 70 * Math.random()}%,
     ${85 + 10 * Math.random()}%
-  )`
-);
+  )`;
 
 const childStyle = () => ({
   minWidth: '100px',
@@ -18,22 +17,10 @@ const childStyle = () => ({
   border: 'solid 1px #333',
 });
 
-const children = [
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-  <div style={ { ...childStyle() } } />,
-];
-
+const childrenCount = new Array(10).fill(0);
+const children = childrenCount.map(() => <div style={{ ...childStyle() }} />);
 
 storiesOf('ColumnsLayout', module)
-  .add('2 columns', () => <ColumnsLayout columnsCount={ 2 }>{ children.map(child => child) }</ColumnsLayout>)
-  .add('3 columns', () => <ColumnsLayout columnsCount={ 3 }>{ children.map(child => child) }</ColumnsLayout>)
-  .add('4 columns', () => <ColumnsLayout columnsCount={ 4 }>{ children.map(child => child) }</ColumnsLayout>)
-;
+  .add('2 columns', () => <ColumnsLayout columnsCount={2}>{children.map(child => child)}</ColumnsLayout>)
+  .add('3 columns', () => <ColumnsLayout columnsCount={3}>{children.map(child => child)}</ColumnsLayout>)
+  .add('4 columns', () => <ColumnsLayout columnsCount={4}>{children.map(child => child)}</ColumnsLayout>);
