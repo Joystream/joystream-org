a341a49b695d47c1cc2bb5b51edecfeab80067d2
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-11 10:46:45 +0200

    Extend TitleWrapper with subtitle

--- src/components/TitleWrapper/titleWrapper.stories.js
+++ src/components/TitleWrapper/titleWrapper.stories.js
@@ -3,10 +3,24 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import TitleWrapper from './index';
+import Link from '../Link';
 
 storiesOf('TitleWrapper', module)
   .add('default', () => (
     <TitleWrapper title="Testnet Goals">
+      <p>Content</p>
+    </TitleWrapper>
+  ))
+  .add('with subtitle', () => (
+    <TitleWrapper
+      title="Testnet Goals"
+      subtitle={
+        <>
+          The goals below is a simplified representation of the Key Results listed in our
+          <Link href="/#"> Release OKR</Link>
+        </>
+      }
+    >
       <p>Content</p>
     </TitleWrapper>
   ));
