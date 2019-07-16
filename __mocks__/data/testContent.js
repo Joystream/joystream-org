9752f38bbdd5b2b34a1c3c0d31f76df7b2d0d9ae
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-07-16 16:11:17 +0200

    Add testContent.js with mockup API data for tests

---
+++ __mocks__/data/testContent.js
@@ -0,0 +1,35 @@
+export default {
+  block_height: 1110117,
+  council: {
+    election_stage: 'Announcing',
+    members_count: 12,
+  },
+  forum: {
+    posts: 11,
+    threads: 7,
+  },
+  media: {
+    media_files: 9,
+  },
+  memberships: {
+    platform_members: 164,
+  },
+  roles: {
+    storage_providers: 10,
+  },
+  runtime_version: {
+    impl_name: 'joystream-node',
+    spec_name: 'joystream-node',
+    spec_version: 4,
+  },
+  system: {
+    chain: 'Joystream Testnet v2',
+    name: 'joystream-node',
+    peerCount: 29,
+    version: '1.0.0',
+  },
+  validators: {
+    count: 20,
+    total_stake: '45658',
+  },
+};
--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -3,41 +3,7 @@ import renderer from 'react-test-renderer';
 
 import { IndexPage } from '../../src/pages';
 
-const testContent = {
-  block_height: 1110117,
-  council: {
-    election_stage: 'Announcing',
-    members_count: 12,
-  },
-  forum: {
-    posts: 11,
-    threads: 7,
-  },
-  media: {
-    media_files: 9,
-  },
-  memberships: {
-    platform_members: 164,
-  },
-  roles: {
-    storage_providers: 10,
-  },
-  runtime_version: {
-    impl_name: 'joystream-node',
-    spec_name: 'joystream-node',
-    spec_version: 4,
-  },
-  system: {
-    chain: 'Joystream Testnet v2',
-    name: 'joystream-node',
-    peerCount: 29,
-    version: '1.0.0',
-  },
-  validators: {
-    count: 20,
-    total_stake: '45658',
-  },
-};
+import testContent from '../../__mocks__/data/testContent';
 
 describe('IndexPage page', () => {
   it('renders correctly', () => {
