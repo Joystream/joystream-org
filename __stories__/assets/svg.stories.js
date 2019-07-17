e9af07bdfb79393f057abdddd75bf497b5b2118e
Maciej Sykuła <maciek.sykula@gmail.com>
2019-07-17 10:57:11 +0200

    Update svg stories with all in one view story

--- __stories__/assets/svg.stories.js
+++ __stories__/assets/svg.stories.js
@@ -3,6 +3,9 @@ import centered from '@storybook/addon-centered/react';
 import { storiesOf } from '@storybook/react';
 
 import importAll from '../../src/utils/importAll';
+import parsePathToName from '../../src/utils/parsePathToName';
+
+import ColumnsLayout from '../../src/components/ColumnsLayout';
 
 const iconStyle = { height: '50vh', maxWidth: '100%' };
 
@@ -15,7 +18,32 @@ stories.addParameters({
   backgrounds: [{ name: 'silver', value: '#cacaca', default: true }],
 });
 
+stories.add('_all', () => {
+  return (
+    <ColumnsLayout columnsCount={4} style={{ padding: '20px' }}>
+      {icons.map((icon, i) => {
+        const Icon = icon.ReactComponent;
+        return (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Icon style={{ maxHeight: '200px', width: '100%' }} />
+            <p style={{ marginTop: '12px' }}>{parsePathToName(iconsList.keys()[i])}</p>
+          </div>
+        );
+      })}
+    </ColumnsLayout>
+  );
+});
+
 icons.forEach((icon, i) => {
   const Icon = icon.ReactComponent;
-  stories.add(`${iconsList.keys()[i]}`, () => <Icon style={iconStyle} />);
+
+  stories.add(`${parsePathToName(iconsList.keys()[i])}`, () => <Icon style={iconStyle} />);
 });
