3d70a86ef48112248f0b3fba6283a0032d38aab3
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-07-19 13:50:05 +0200

    Remove fake goalsData page

--- src/data/pages/athens.js
+++ src/data/pages/athens.js
@@ -1,15 +1,7 @@
+import React from 'react';
 import { ReactComponent as ValidatorsImage } from '../../assets/svg/active-validators.svg';
 import { ReactComponent as StorageImage } from '../../assets/svg/storage.svg';
 import { ReactComponent as MemberImage } from '../../assets/svg/council-member.svg';
-import { ReactComponent as ScreenerImage } from '../../assets/svg/screener.svg';
-import { ReactComponent as MembershipCuratorImage } from '../../assets/svg/membership-curator.svg';
-import { ReactComponent as ContentCuratorImage } from '../../assets/svg/content-curator.svg';
-import { ReactComponent as ContentCreatorImage } from '../../assets/svg/content-creator.svg';
-import { ReactComponent as BandwidthProviderImage } from '../../assets/svg/bandwidth-provider.svg';
-import { ReactComponent as DiscoveryProviderImage } from '../../assets/svg/discovery-provider.svg';
-import { ReactComponent as LiveStreamingProviderImage } from '../../assets/svg/live-streaming-provider.svg';
-import { ReactComponent as BuilderImage } from '../../assets/svg/builder.svg';
-import { ReactComponent as CommunicationModeratorImage } from '../../assets/svg/communication-moderator.svg';
 import { ReactComponent as payoutImage } from '../../assets/svg/participation-payout.svg';
 import { ReactComponent as blockImage } from '../../assets/svg/block-platform-content-files.svg';
 import { ReactComponent as bookImage } from '../../assets/svg/platform-content-files.svg';
@@ -54,4 +46,44 @@ const analytics = [
   { title: 'Block Height', image: blockImage, key: 'blockHeight' },
 ];
 
-export { roles, analytics };
+const goals = [
+  {
+    title: 'Enable media on the platform',
+    text:
+      /* eslint-disable */
+      'As a "user governed media platform", allowing users to upload and consume and content was a major goal for us.',
+    /* eslint-disable */
+  },
+  {
+    title: 'Introduce platform memberships',
+    text:
+      /* eslint-disable */
+      'Although we are building an open blockchain in the sense that users are free to send tokens as they like, the Joystream experience is about having the platform users own, operate and govern the platform. We are agnostic on exactly what parts of the platform will be reserved for members, some actions will require and extra level of screening and accountability.',
+    /* eslint-disable */
+  },
+  {
+    title: 'Build and release the storage node',
+    text:
+      /* eslint-disable */
+      'Although a storage node was built and used to host and distribute the platform content, it had some bugs making it unable to sync between clients and required a hardcoded liaison.',
+    /* eslint-disable */
+    state: 'postponed',
+  },
+  {
+    title: 'Upgrade the runtime through a Council vote',
+    /* eslint-disable */
+    text: [
+      'The intention was for Jsgenesis to create a proposal for a ',
+      <a href="https://blog.joystream.org/upgrades/">runtime upgrade</a>,
+      ', and have the Council vote on it. If the Council reached quorum, the consensus rules of the system would automatically get upgraded in flight. Unfortunately, the ',
+      <a href="https://blog.joystream.org/sparta-sacked/">
+        Sparta network crashed
+      </a>,
+      ' before we reached this stage, and had to start Athens as a new chain with a new genesis block.',
+    ],
+    /* eslint-disable */
+    state: 'postponed',
+  },
+];
+
+export { roles, analytics, goals };
