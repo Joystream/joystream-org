9b7dddf0faa377d1a87c41272a3b6fc342934d2e
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-07-26 16:27:01 +0200

    Connect Sidebar with ScrollContext

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import cn from 'classnames';
 import { object, func, string } from 'prop-types';
 
+import { ScrollConsumer } from '../_enhancers/ScrollContext';
+
 import { ReactComponent as MoreRolesIcon } from '../../assets/svg/arrow-down.svg';
 import { ReactComponent as TickImage } from '../../assets/svg/tick.svg';
 import { ReactComponent as UpcomingImage } from '../../assets/svg/upcoming.svg';
@@ -34,54 +36,62 @@ class Sidebar extends React.Component {
     const { data, onElementChange } = this.props;
 
     return (
-      <aside className="Sidebar ">
-        <div
-          className={cn('Sidebar__trigger', {
-            'Sidebar__trigger--active': isOpen,
-          })}
-          onClick={this.toggleMenu}
-          role="presentation"
-        >
-          <MoreRolesIcon className="Sidebar__trigger-icon" />
-          View all Roles
-        </div>
-        <div className="Sidebar__wrapper">
-          <div
-            className={cn('Sidebar__container', {
-              'Sidebar__container--open': isOpen,
+      <ScrollConsumer>
+        {({ isScrollUp }) => (
+          <aside
+            className={cn('Sidebar', {
+              'Sidebar--onTop': !isScrollUp,
             })}
           >
-            {Object.keys(data).map(key => {
-              const Icon = key === 'active' ? TickImage : UpcomingImage;
-              const title =
-                key === 'active' ? 'Active roles' : 'Upcoming roles';
+            <div
+              className={cn('Sidebar__trigger', {
+                'Sidebar__trigger--active': isOpen,
+              })}
+              onClick={this.toggleMenu}
+              role="presentation"
+            >
+              <MoreRolesIcon className="Sidebar__trigger-icon" />
+              View all Roles
+            </div>
+            <div className="Sidebar__wrapper">
+              <div
+                className={cn('Sidebar__container', {
+                  'Sidebar__container--open': isOpen,
+                })}
+              >
+                {Object.keys(data).map(key => {
+                  const Icon = key === 'active' ? TickImage : UpcomingImage;
+                  const title =
+                    key === 'active' ? 'Active roles' : 'Upcoming roles';
 
-              return (
-                <div className="Sidebar__group" key={title}>
-                  <div className="Sidebar__heading">
-                    <Icon className="Sidebar__state-icon" />
-                    <p className="Sidebar__title">{title}</p>
-                  </div>
+                  return (
+                    <div className="Sidebar__group" key={title}>
+                      <div className="Sidebar__heading">
+                        <Icon className="Sidebar__state-icon" />
+                        <p className="Sidebar__title">{title}</p>
+                      </div>
 
-                  {data[key].map(({ title }) => (
-                    <button
-                      className={cn('Sidebar__link', {
-                        'Sidebar__link--active': activeLink === title,
-                      })}
-                      key={title}
-                      onClick={() => {
-                        onElementChange(title);
-                      }}
-                    >
-                      {title}
-                    </button>
-                  ))}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </aside>
+                      {data[key].map(({ title }) => (
+                        <button
+                          className={cn('Sidebar__link', {
+                            'Sidebar__link--active': activeLink === title,
+                          })}
+                          key={title}
+                          onClick={() => {
+                            onElementChange(title);
+                          }}
+                        >
+                          {title}
+                        </button>
+                      ))}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </aside>
+        )}
+      </ScrollConsumer>
     );
   }
 }
