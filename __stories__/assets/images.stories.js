01b67509e52c95324fb7e111aa8f9eca1bdcc5d8
Maciej Sykuła <maciek.sykula@gmail.com>
2019-08-01 09:44:43 +0200

    Add favicon image

--- __stories__/assets/images.stories.js
+++ __stories__/assets/images.stories.js
@@ -4,10 +4,12 @@ import { storiesOf } from '@storybook/react';
 
 import AthensImage from '../../src/assets/images/athens.png';
 import SpartaImage from '../../src/assets/images/sparta.png';
+import FaviconImage from '../../src/assets/images/favicon.png';
 
 const imageStyle = { height: '50vh', maxWidth: '100%' };
 
 storiesOf('Assets|Images', module)
   .addDecorator(centered)
   .add('Athens map', () => <img src={AthensImage} alt="Athens" style={imageStyle} />)
-  .add('Sparta map', () => <img src={SpartaImage} alt="Sparta" style={imageStyle} />);
+  .add('Sparta map', () => <img src={SpartaImage} alt="Sparta" style={imageStyle} />)
+  .add('favicon', () => <img src={FaviconImage} alt="JoyStream" style={imageStyle} />);
