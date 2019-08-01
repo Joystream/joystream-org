34ab1a5f203e55be55681e0599ccaa765eac51a6
Klaudia Bęczkowska <klaudia.beczkowska@elpassion.pl>
2019-08-01 12:24:39 +0200

    Add Role Page test

---
+++ __tests__/pages/roles.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+
+// import RolesPage from '../../src/pages/roles';
+
+describe('RolesPage page', () => {
+  it('renders correctly', () => {
+    // const tree = renderer.create(<RolesPage />).toJSON();
+    // expect(tree).toMatchSnapshot();
+  });
+});
