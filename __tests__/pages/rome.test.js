542a60bd2705294b98c359559749e8461634c6c9
maciejmatu <maciek.matuszewski@gmail.com>
2019-10-21 14:41:37 +0200

    Remove redundant code from rome.test.js

--- __tests__/pages/rome.test.js
+++ __tests__/pages/rome.test.js
@@ -1,11 +1,8 @@
-import { configure, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
 import { RomePage } from '../../src/pages/rome';
 import testContent from '../../__mocks__/data/testContent';
-
-configure({ adapter: new Adapter() });
 
 describe('RomePage page', () => {
   it('renders correctly', () => {
