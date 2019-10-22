c43b08980effb8e5254a65d694c80b351ec047af
maciejmatu <maciek.matuszewski@gmail.com>
2019-10-22 16:40:52 +0200

    Update imageSection stories

--- src/components/ImageSection/imageSection.stories.js
+++ src/components/ImageSection/imageSection.stories.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import centered from '@storybook/addon-centered/react';
 import { storiesOf } from '@storybook/react';
 import ImageSection from './';
 
-import goalImage from '../../assets/svg/goal.svg';
+import data from '../../data/pages/manifesto';
 
 storiesOf('Components|ImageSection', module)
-  .addDecorator(centered)
   .add('deafult', () => (
-    <ImageSection title="Goal" image={goalImage}>
-      We call for an arrangement where media platforms are accountable to the people they impact, which are primarily
-      their users, be it as consumers, creatives, third party developer or workers.
-      <blockquote>
-        It is the absence of such accountability which is the fundamental source of any particular problem at any
-        particular time.
-      </blockquote>
+    <ImageSection title={data.sections.problem.title} image={data.sections.problem.image}>
+      {data.sections.problem.text}
     </ImageSection>
+  ))
+  .add('grouped', () => (
+    <div>
+      <ImageSection title={data.groupSections.wedge.title} image={data.groupSections.wedge.image} grouped>
+        {data.groupSections.wedge.text}
+      </ImageSection>
+      <ImageSection title={data.groupSections.voice.title} image={data.groupSections.voice.image} grouped>
+        {data.groupSections.voice.text}
+      </ImageSection>
+    </div>
   ));
