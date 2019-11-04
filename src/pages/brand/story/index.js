04c0a6ed6921e36fd788a6b70f578f978931f163
maciejmatu <maciek.matuszewski@gmail.com>
2019-11-04 15:20:57 +0100

    Add Text under website section

--- src/pages/brand/story/index.js
+++ src/pages/brand/story/index.js
@@ -7,6 +7,7 @@ import LayoutWrapper from '../../../components/LayoutWrapper';
 import laptopImg from '../../../assets/images/laptop-website.png';
 import wavesImg from '../../../assets/svg/waves.svg';
 import logoImg from '../../../assets/svg/logo-mark.svg';
+import ColumnsLayout from '../../../components/ColumnsLayout';
 import './index.scss';
 
 const StoryPage = () => {
@@ -49,6 +50,45 @@ const StoryPage = () => {
             <img src={laptopImg} className="StoryWebsite__image" alt="" />
           </div>
         </div>
+      </LayoutWrapper>
+
+      <LayoutWrapper className="StoryDescription">
+        <p className="StoryDescription__header">
+          Our brand identity consists of few key visual elements: logo, typeface, illustrations, patter, colour palette
+          and iconography.
+        </p>
+        <ColumnsLayout className="StoryDescription__text">
+          <div>
+            <p>
+              Each of them has a meaningful role in brand identity as established in the guidelines. But combining all
+              of these elements builds much more than just a visual experience.
+            </p>
+            <p>
+              Joystream branding is a powerful tool of communication with all of its users throughout the whole
+              experience.
+            </p>
+            <p>
+              Fundamentally, our effort is an experimental one; we do not have all the answers, and we want to be honest
+              about that in our communication.
+            </p>
+          </div>
+          <div>
+            <p>
+              We are not promising cheaper, faster or nicer, like one typically would when selling a consumer widget or
+              service. Instead we have hope that we can build something more empowering and accountable through this
+              experimentation.{' '}
+            </p>
+            <p>
+              Lastly, at the core of our vision is governance, so what we are building is never finished; we are only
+              constructing the first piece. It is a dynamic effort we hope communities will carry forward in amazing
+              ways, we are just providing the first set of tools.
+            </p>
+          </div>
+        </ColumnsLayout>
+        <p className="StoryDescription__footer">
+          So our brand should carry this feeling of experimentation, excitement and building something ethical and
+          dynamic.
+        </p>
       </LayoutWrapper>
     </BrandLayout>
   );
