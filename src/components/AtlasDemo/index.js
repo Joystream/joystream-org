90bb49cab6de745bc73f66604b323b553fdfff0e
Edvin <edvindzidic2000@gmail.com>
2020-11-15 15:51:26 +0100

    Remove unnecessary elements, add the rest of the animations

--- src/components/AtlasDemo/index.js
+++ src/components/AtlasDemo/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import './style.scss';
 import Button from '../Button';
-import Announcing from '../../assets/svg/announcing-white.svg';
+import BackgroundLeft from '../../assets/svg/atlas-bg-shapes-left.svg';
+import BackgroundRight from '../../assets/svg/atlas-bg-shapes-right.svg';
 import AtlasVideo from './AtlasVideo';
 
-const AtlasDemo = ({}) => {
+const AtlasDemo = () => {
     return(
       <div className="AtlasDemo">
+        <img src={BackgroundLeft} alt="" className="AtlasDemo__background-left"></img>
+        <img src={BackgroundRight} alt="" className="AtlasDemo__background-right"></img>
         <div className="AtlasDemo__container">
           <AtlasVideo />
-          <p className="AtlasDemo__paragraph">Try out Atlas for the first time, enjoy unlimited content on all of your devices.</p>
-          <div className="AtlasDemo__buttons">
-            <Button secondary large >Try out Atlas</Button>
-            <Button secondary large reversed>Learn more</Button>
-          </div>
-          <div className="AtlasDemo__announcement">
-            <img src={Announcing} alt="Megaphone logo."/>
-            <p className="AtlasDemo__announcement__info">You can also join our Newsletter and stay up to date:</p>
-            <div className="AtlasDemo__announcement__form">
-              <input placeholder="email address.." type="text" />
-              <button>Join Now</button>
-            </div>
-          </div>
+          <Button className="AtlasDemo__try-out-button" secondary large >Try It Out</Button>
         </div>
       </div>
     );
