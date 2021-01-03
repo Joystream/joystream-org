74054ebc2e2bcc0a4230a7c52af15e6b8fa1c4f7
Ben Holden-Crowther <4144334+blrhc@users.noreply.github.com>
2021-01-03 21:36:09 +0000

    Updated Replenishment

--- src/data/pages/testnet.js
+++ src/data/pages/testnet.js
@@ -40,7 +40,7 @@ export default {
                 effectively increasing the exchange rate (all else being equal).
               </p>
               <p>
-                Last replenishment: <i>$600 on December 19th</i>
+                Last replenishment: <i>$600 on January 2nd</i>
               </p>
             </blockquote>
           </>
