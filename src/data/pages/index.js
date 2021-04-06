7219b68b421922a30179ee0dfe6d4c01ca2c7fd9
Edvin <edvindzidic2000@gmail.com>
2021-04-06 14:17:05 +0200

    Update antioch date, remove banner, update Road to Mainnet, update babylon hero card

--- src/data/pages/index.js
+++ src/data/pages/index.js
@@ -162,7 +162,7 @@ export const testnetData = [
     number: 8,
     name: 'Antioch',
     date: '07.04.2021',
-    state: 'Future',
+    state: 'Current',
     overviewText: 'Antioch is our ninth testnet, patching a chain split bug which broke the Babylon network',
     link: '/antioch'
   },
@@ -171,7 +171,7 @@ export const testnetData = [
     number: 7,
     name: 'Babylon',
     date: '21.12.2020',
-    state: 'Current',
+    state: 'Past',
     overviewText: 'Babylon is our eighth testnet, introducing a variety of important technical improvements.',
     link: '/babylon',
   },
