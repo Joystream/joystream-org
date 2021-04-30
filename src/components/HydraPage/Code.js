577ec3bb435e0c4cad333337b76e2a767e599507
Edvin <edvindzidic2000@gmail.com>
2021-04-30 12:18:07 +0200

    Remove console.log from Code.js

--- src/components/HydraPage/Code.js
+++ src/components/HydraPage/Code.js
@@ -4,7 +4,6 @@ import './syntax.scss';
 
 export default function Code({ code, lang }) {
   useEffect(() => Prism.highlightAll());
-  console.log(code, lang)
   return (
     <pre>
       <code className={`language-${lang}`}>{code}</code>
