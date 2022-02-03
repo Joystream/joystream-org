237c20442a8ec8c71e5103d50e7d6c8af756f7e0
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-02-04 00:20:21 +0100

    [WIP] Joystream Onboarding UI: Founding Member Advice

--- src/components/onboarding-page/FMInterviews/InterviewSection/index.js
+++ src/components/onboarding-page/FMInterviews/InterviewSection/index.js
@@ -1,63 +1,20 @@
-import React, { useState, useRef, useEffect } from 'react';
-import cn from 'classnames';
-import Play from '../../../../assets/svg/btn-play.svg';
-import Loader from 'react-loader-spinner';
+import React from 'react';
 import './style.scss';
 
-const InterviewSection = ({ title, subtitle, videoUrl, thumbnail }) => {
-  const [videoIsHovered, setVideoIsHovered] = useState(false);
-  const [imageIsLoading, setImageIsLoading] = useState(false);
-  const videoThumbnailRef = useRef();
-
-  useEffect(() => {
-    let preloaderImg = document.createElement('img');
-    preloaderImg.src = thumbnail;
-
-    preloaderImg.addEventListener('load', event => {
-      videoThumbnailRef.current.style.backgroundImage = `url(${thumbnail})`;
-      setImageIsLoading(false);
-      preloaderImg = null;
-    });
-  }, [thumbnail]);
-
+const InterviewSection = ({ title, subtitle, image, fullWidth }) => {
   return (
-    <div className="InterviewSection__hero-wrapper">
-      <div className="InterviewSection__hero">
-        <div className="InterviewSection__hero__content">
-          <h1 className="InterviewSection__hero__title">{title}</h1>
-          <h2 className="InterviewSection__hero__subtitle">{subtitle}</h2>
-        </div>
-        <div className="InterviewSection__hero__content">
-          <div
-            ref={videoThumbnailRef}
-            role="presentation"
-            className="InterviewSection__hero__video"
-            onMouseEnter={() => setVideoIsHovered(true)}
-            onMouseLeave={() => setVideoIsHovered(false)}
-          >
-            {imageIsLoading && (
-              <Loader
-                className="InterviewSection__hero__video__loader"
-                type="Oval"
-                color="#302ABF"
-                height="100%"
-                width="100%"
-                timeout={0}
-              />
-            )}
-            <img
-              role="presentation"
-              onClick={() => {}}
-              className={cn('InterviewSection__hero__video__playbutton', {
-                'InterviewSection__hero__video__playbutton--hovered': videoIsHovered,
-              })}
-              src={Play}
-              alt="Play button"
-            />
-            {videoIsHovered && <div className="InterviewSection__hero__video__backdrop" />}
-          </div>
-        </div>
-      </div>
+    <div className={`InterviewSection__hero${fullWidth ? ' InterviewSection__hero__full-width' : ''}`}>
+      <h1 className="InterviewSection__hero__title">
+        <img className="InterviewSection__hero__image" src={image} alt="" />
+        {title}
+      </h1>
+      <h2
+        className={`InterviewSection__hero__subtitle${
+          fullWidth ? ' InterviewSection__hero__subtitle__full-width' : ''
+        }`}
+      >
+        {subtitle}
+      </h2>
     </div>
   );
 };
--- src/components/onboarding-page/FMInterviews/index.js
+++ src/components/onboarding-page/FMInterviews/index.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import InterviewSection from './InterviewSection';
-import Thumbnail1 from '../../../assets/images/interview-thumbnail-1.png';
-import Thumbnail2 from '../../../assets/images/interview-thumbnail-2.png';
+import FMAdvice1 from '../../../assets/images/fm-advice-1.png';
+import FMAdvice2 from '../../../assets/images/fm-advice-2.png';
+import FMAdvice3 from '../../../assets/images/fm-advice-3.png';
+import FMAdvice4 from '../../../assets/images/fm-advice-4.png';
+import FMAdvice5 from '../../../assets/images/fm-advice-5.png';
 
 import './style.scss';
 
@@ -10,32 +13,52 @@ const FMInterviews = ({ t }) => {
     {
       title: t('onboarding.page3.fmInterviews.section1.title'),
       subtitle: t('onboarding.page3.fmInterviews.section1.text'),
-      thumbnail: Thumbnail1,
-      videoUrl: 'todo.mp4',
+      image: FMAdvice1,
+      fullWidth: false,
     },
     {
       title: t('onboarding.page3.fmInterviews.section2.title'),
       subtitle: t('onboarding.page3.fmInterviews.section2.text'),
-      thumbnail: Thumbnail2,
-      videoUrl: 'todo.mp4',
+      image: FMAdvice2,
+      fullWidth: false,
+    },
+    {
+      title: t('onboarding.page3.fmInterviews.section3.title'),
+      subtitle: t('onboarding.page3.fmInterviews.section3.text'),
+      image: FMAdvice3,
+      fullWidth: true,
+    },
+    {
+      title: t('onboarding.page3.fmInterviews.section4.title'),
+      subtitle: t('onboarding.page3.fmInterviews.section4.text'),
+      image: FMAdvice4,
+      fullWidth: false,
+    },
+    {
+      title: t('onboarding.page3.fmInterviews.section5.title'),
+      subtitle: t('onboarding.page3.fmInterviews.section5.text'),
+      image: FMAdvice5,
+      fullWidth: false,
     },
   ];
 
   return (
     <div className="FMInterviews__wrapper">
       <h2 className="FMInterviews__title">{t('onboarding.page3.fmInterviews.title')}</h2>
-      {data &&
-        data.map((item, index) => {
-          return (
-            <InterviewSection
-              key={index}
-              title={item.title}
-              subtitle={item.subtitle}
-              thumbnail={item.thumbnail}
-              videoUrl={item.videoUrl}
-            />
-          );
-        })}
+      <div className="FMInterviews__item__wrapper">
+        {data &&
+          data.map((item, index) => {
+            return (
+              <InterviewSection
+                key={index}
+                title={item.title}
+                subtitle={item.subtitle}
+                image={item.image}
+                fullWidth={item.fullWidth}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
