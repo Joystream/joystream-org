81cc1f1f06aff97541e9c9e63b28114abf90e459
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-02-04 14:41:45 +0100

    [WIP] Joystream Onboarding UI: Founding Member How To Vertical Scroll Animation

--- src/components/onboarding-page/BecomeFM/index.js
+++ src/components/onboarding-page/BecomeFM/index.js
@@ -1,22 +1,11 @@
-import React from 'react';
-
+import React, { useState, useRef, useEffect } from 'react';
+import useScroll from '../../../utils/useScroll';
+import useWindowDimensions from '../../../utils/useWindowDimensions';
 import './style.scss';
 
-const NumberStep = ({ number, text, otherText, isFirst, isMiddle, isLast }) => {
+const NumberStep = ({ text, otherText }) => {
   return (
     <div className="BecomeFM__number-step">
-      <div className="BecomeFM__number-step__number__wrapper">
-        <div className={`BecomeFM__number-step__number ${isLast ? 'BecomeFM__number-step__number--white' : ''}`}>
-          {number}
-        </div>
-        {isFirst && <div className="BecomeFM__number-step__line"></div>}
-        {isMiddle && (
-          <>
-            <div className="BecomeFM__number-step__line-half-blue"></div>
-            <div className="BecomeFM__number-step__line-half-white"></div>
-          </>
-        )}
-      </div>
       <div className="BecomeFM__number-step__texts">
         <p className="BecomeFM__number-step__text">{text}</p>
         <p className="BecomeFM__number-step__other-text">{otherText ?? text}</p>
@@ -26,27 +15,47 @@ const NumberStep = ({ number, text, otherText, isFirst, isMiddle, isLast }) => {
 };
 
 const BecomeFM = ({ t }) => {
-  return (
-    <div className="BecomeFM__wrapper">
-      <div className="BecomeFM__content">
-        <h2 className="BecomeFM__title">{t('onboarding.page3.fmSteps.title')}</h2>
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const componentRef = useRef();
+  const { scrollPosition } = useScroll();
+  const { height } = useWindowDimensions();
 
+  useEffect(() => {
+    if (
+      scrollPosition &&
+      componentRef?.current?.offsetTop &&
+      height &&
+      scrollPosition >= componentRef.current.offsetTop - height
+    ) {
+      setShouldAnimate(true);
+    }
+  }, [scrollPosition, height]);
+
+  return (
+    <div ref={componentRef} className="BecomeFM__wrapper">
+      <div className="BecomeFM__title__wrapper">
+        <h2 className="BecomeFM__title">{t('onboarding.page3.fmSteps.title')}</h2>
+      </div>
+      <div className="BecomeFM__content">
+        <div
+          className={`BecomeFM__howto__number-steps ${shouldAnimate ? 'BecomeFM__howto__number-steps--animated' : ''}`}
+        >
+          <div className="BecomeFM__howto__number-step">{t('numbers.one')}</div>
+          <div className="BecomeFM__howto__number-step-separator BecomeFM__howto__number-step-separator--active"></div>
+          <div className="BecomeFM__howto__number-step">{t('numbers.two')}</div>
+          <div className="BecomeFM__howto__number-step-separator BecomeFM__howto__number-step-separator--long"></div>
+          <div className="BecomeFM__howto__number-step">{t('numbers.three')}</div>
+        </div>
         <div className="BecomeFM__number-steps">
           <NumberStep
-            isFirst
-            number={t('numbers.one')}
             text={t('onboarding.page3.fmSteps.step1.title')}
             otherText={t('onboarding.page3.fmSteps.step1.text')}
           />
           <NumberStep
-            isMiddle
-            number={t('numbers.two')}
             text={t('onboarding.page3.fmSteps.step2.title')}
             otherText={t('onboarding.page3.fmSteps.step2.text')}
           />
           <NumberStep
-            isLast
-            number={t('numbers.three')}
             text={t('onboarding.page3.fmSteps.step3.title')}
             otherText={t('onboarding.page3.fmSteps.step3.text')}
           />
