5f58377864d57d3febe68af04f4b765d5feeaf92
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-02-11 00:30:45 +0100

    [WIP] Joystream Onboarding UI: Lottie animation POC - Sticky Animation

--- src/components/onboarding-page/Integrators/index.js
+++ src/components/onboarding-page/Integrators/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import Lottie from 'react-lottie';
 import { ReactComponent as Arrow } from '../../../assets/svg/arrow-down-small.svg';
 import { ReactComponent as OnlineIcon } from '../../../assets/svg/integrators-online-icon.svg';
@@ -25,87 +25,104 @@ const Integrators = ({ t, onChatWithIntegrator }) => {
   return (
     <div className="Integrators__wrapper">
       <div className="Integrators__content">
-        <div className="Integrators__animation">
-          <Waypoint onEnter={() => setShouldAnimateFirst(true)} />
-          {shouldAnimateFirst && (
-            <Lottie options={{ ...defaultOptions, animationData: animationData1 }} height={340} width={520} />
-          )}
-        </div>
-        <div className="Integrators__text">
-          <div className="Integrators__online-text">
-            <OnlineIcon className="Integrators__online-icon" />
-            {t('onboarding.integrators.online')}
+        <div className="Integrators__animation__wrapper">
+          <div className="Integrators__animation">
+            {shouldAnimateFirst && (
+              <Lottie options={{ ...defaultOptions, animationData: animationData1 }} height={340} width={520} />
+            )}
+            {shouldAnimateSecond && (
+              <Lottie options={{ ...defaultOptions, animationData: animationData2 }} height={340} width={520} />
+            )}
+            {shouldAnimateThird && (
+              <Lottie options={{ ...defaultOptions, animationData: animationData3 }} height={340} width={520} />
+            )}
           </div>
-          <h2 className="Integrators__title">
-            {t('onboarding.integrators.section1.title')
-              .split('<br/>')
-              .map((line, index) => {
-                return (
-                  <span key={index}>
-                    {line}
-                    <br />
-                    <br />
-                  </span>
-                );
-              })}
-          </h2>
         </div>
-      </div>
-      <div className="Integrators__content">
-        <div className="Integrators__animation">
-          <Waypoint onEnter={() => setShouldAnimateSecond(true)} />
-          {shouldAnimateSecond && (
-            <Lottie options={{ ...defaultOptions, animationData: animationData2 }} height={340} width={520} />
-          )}
-        </div>
-        <div className="Integrators__text">
-          <div className="Integrators__online-text">
-            <OnlineIcon className="Integrators__online-icon" />
-            {t('onboarding.integrators.online')}
+        <div className="Integrators__text__wrapper">
+          <div className="Integrators__text">
+            <div className="Integrators__online-text">
+              <OnlineIcon className="Integrators__online-icon" />
+              {t('onboarding.integrators.online')}
+            </div>
+            <h2 className="Integrators__title">
+              {t('onboarding.integrators.section1.title')
+                .split('<br/>')
+                .map((line, index) => {
+                  return (
+                    <span key={index}>
+                      {line}
+                      <br />
+                      <br />
+                    </span>
+                  );
+                })}
+            </h2>
+            <Waypoint
+              onEnter={() => {
+                setShouldAnimateThird(false);
+                setShouldAnimateSecond(false);
+                setShouldAnimateFirst(true);
+              }}
+            />
           </div>
-          <h2 className="Integrators__title">
-            {t('onboarding.integrators.section2.title')
-              .split('<br/>')
-              .map((line, index) => {
-                return (
-                  <span key={index}>
-                    {line}
-                    <br />
-                    <br />
-                  </span>
-                );
-              })}
-          </h2>
-        </div>
-      </div>
-      <div className="Integrators__content">
-        <div className="Integrators__animation">
-          <Waypoint onEnter={() => setShouldAnimateThird(true)} />
-          {shouldAnimateThird && (
-            <Lottie options={{ ...defaultOptions, animationData: animationData3 }} height={340} width={520} />
-          )}
-        </div>
-        <div className="Integrators__text">
-          <div className="Integrators__online-text">
-            <OnlineIcon className="Integrators__online-icon" />
-            {t('onboarding.integrators.online')}
+          <div className="Integrators__text">
+            <div className="Integrators__online-text">
+              <OnlineIcon className="Integrators__online-icon" />
+              {t('onboarding.integrators.online')}
+            </div>
+            <h2 className="Integrators__title">
+              {t('onboarding.integrators.section2.title')
+                .split('<br/>')
+                .map((line, index) => {
+                  return (
+                    <span key={index}>
+                      {line}
+                      <br />
+                      <br />
+                    </span>
+                  );
+                })}
+            </h2>
+            <Waypoint
+              className="IntegratorsclassName"
+              bottomOffset={500}
+              onEnter={() => {
+                setShouldAnimateFirst(false);
+                setShouldAnimateThird(false);
+                setShouldAnimateSecond(true);
+              }}
+            />
           </div>
-          <h2 className="Integrators__title">
-            {t('onboarding.integrators.section3.title')
-              .split('<br/>')
-              .map((line, index) => {
-                return (
-                  <span key={index}>
-                    {line}
-                    <br />
-                    <br />
-                  </span>
-                );
-              })}
-          </h2>
-          <div className="Integrators__button" role="presentation" onClick={onChatWithIntegrator}>
-            <p className="Integrators__button-text">{t('onboarding.integrators.button')}</p>
-            <Arrow className="Integrators__button-arrow" />
+          <div className="Integrators__text">
+            <div className="Integrators__online-text">
+              <OnlineIcon className="Integrators__online-icon" />
+              {t('onboarding.integrators.online')}
+            </div>
+            <h2 className="Integrators__title">
+              {t('onboarding.integrators.section3.title')
+                .split('<br/>')
+                .map((line, index) => {
+                  return (
+                    <span key={index}>
+                      {line}
+                      <br />
+                      <br />
+                    </span>
+                  );
+                })}
+            </h2>
+            <Waypoint
+              bottomOffset={500}
+              onEnter={() => {
+                setShouldAnimateFirst(false);
+                setShouldAnimateSecond(false);
+                setShouldAnimateThird(true);
+              }}
+            />
+            <div className="Integrators__button" role="presentation" onClick={onChatWithIntegrator}>
+              <p className="Integrators__button-text">{t('onboarding.integrators.button')}</p>
+              <Arrow className="Integrators__button-arrow" />
+            </div>
           </div>
         </div>
       </div>
