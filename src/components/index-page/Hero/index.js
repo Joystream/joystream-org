0beaf041529cbdf3a306e6948fc88535ccbf6886
Edvin Dzidic <dzidic@student.tugraz.at>
2022-08-02 18:15:53 +0200

    Update react-plx, animate text caret in landing hero, finish hero visual implementation with parallax effect

--- src/components/index-page/Hero/index.js
+++ src/components/index-page/Hero/index.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import TypeWriter from 'typewriter-effect';
+import Plx from 'react-plx';
 
 import { ArrowButton } from '../../ArrowButton';
 
 import AtlasHeroBackgroundImage from '../../../assets/images/landing/atlas-hero-background.png';
+import AtlasHeroForegroundImage from '../../../assets/images/landing/atlas-hero-foreground.png';
+import AtlasHeroButtonImage from '../../../assets/images/landing/atlas-hero-button.png';
 
 import './style.scss';
+
+const parallaxDataForeground = [
+  {
+    start: "0px",
+    end: "400px",
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: -720,
+        endValue: -755,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  },
+  {
+    start: 700,
+    end: 1200,
+    easing: 'easeOut',
+    properties: [
+      {
+        startValue: -755,
+        endValue: -855,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  }
+];
+
+const parallaxDataButton = [
+  {
+    start: 0,
+    duration: 400,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: -680,
+        endValue: -760,
+        property: 'translateY',
+      },
+    ],
+  },
+  {
+    start: 700,
+    end: 1200,
+    easing: 'easeOut',
+    properties: [
+      {
+        startValue: -760,
+        endValue: -880,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  },
+];
 
 const Hero = ({ t }) => {
   return (
@@ -19,8 +79,8 @@ const Hero = ({ t }) => {
                 strings: ['creators', 'consumers', 'you', 'me', 'us'],
                 autoStart: true,
                 loop: true,
-                wrapperClassName: "IndexPage__hero__typewriter-title",
-                cursorClassName: "IndexPage__hero__typewriter-cursor"
+                wrapperClassName: 'IndexPage__hero__typewriter-title',
+                cursorClassName: 'IndexPage__hero__typewriter-cursor',
               }}
             />
           </span>
@@ -32,11 +92,27 @@ const Hero = ({ t }) => {
           className="IndexPage__hero__button"
           textClassname="IndexPage__hero__button-text"
         />
-        <img
-          className="IndexPage__hero__image"
-          src={AtlasHeroBackgroundImage}
-          alt="video playing on atlas, next to the video is a bid section for the associated NFT"
-        />
+        <div className="IndexPage__hero__illustration">
+          <img
+            className="IndexPage__hero__illustration__background"
+            src={AtlasHeroBackgroundImage}
+            alt="video playing on atlas"
+          />
+          <Plx parallaxData={parallaxDataForeground} animateWhenNotInViewport={true}>
+            <img
+              className="IndexPage__hero__illustration__foreground"
+              src={AtlasHeroForegroundImage}
+              alt="bid section for the associated video NFT"
+            />
+          </Plx>
+          <Plx parallaxData={parallaxDataButton} animateWhenNotInViewport={true}>
+            <img
+              className="IndexPage__hero__illustration__button"
+              src={AtlasHeroButtonImage}
+              alt="place bid on NFT button"
+            />
+          </Plx>
+        </div>
       </div>
     </div>
   );
