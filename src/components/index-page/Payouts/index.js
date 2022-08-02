a1487a2c79cab1ea6b0b06a8289fd78161e9d901
Edvin Dzidic <dzidic@student.tugraz.at>
2022-08-02 19:53:37 +0200

    Add parallax effect to payouts section

--- src/components/index-page/Payouts/index.js
+++ src/components/index-page/Payouts/index.js
@@ -1,13 +1,99 @@
 import React from 'react';
+import Plx from 'react-plx';
 
 import { ArrowButton } from '../../ArrowButton';
 
 import PayoutsBackgroundImage from '../../../assets/images/landing/payouts-background.png';
+import PayoutsForeground from '../../../assets/images/landing/payouts-foreground.png';
 import { ReactComponent as YoutubeLogo } from '../../../assets/svg/landing/youtube-logo.svg'
 import { ReactComponent as ConnectionIcon } from '../../../assets/svg/landing/connection-icon.svg'
 import { ReactComponent as JoystreamLogo } from '../../../assets/svg/logo-mark.svg';
 
 import './style.scss';
+
+const parallaxDataBackground = [
+  {
+    start: 0,
+    end: 500,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: 100,
+        endValue: 100,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  },
+  {
+    start: 500,
+    end: 900,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: 100,
+        endValue: 0,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  },
+  {
+    start: 1300,
+    end: 1800,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: 0,
+        endValue: -150,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  }
+];
+
+const parallaxDataForeground = [
+  {
+    start: 0,
+    end: 800,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: 600,
+        endValue: 600,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  },
+  {
+    start: 800,
+    end: 1100,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: 600,
+        endValue: 510,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  },
+  {
+    start: 1300,
+    end: 1800,
+    easing: 'easeIn',
+    properties: [
+      {
+        startValue: 510,
+        endValue: 360,
+        property: 'translateY',
+        unit: "px"
+      },
+    ],
+  }
+];
 
 const Payouts = ({ t }) => {
   return (
@@ -22,11 +108,20 @@ const Payouts = ({ t }) => {
           </p>
         </section>
         <div className="IndexPage__payouts__illustration">
-          <img
-            src={PayoutsBackgroundImage}
-            className="IndexPage__payouts__illustration__background"
-            alt="in the background, my payments tab in atlas studio is shown, in the foreground, a claim payout popup"
-          />
+          <Plx parallaxData={parallaxDataBackground} animateWhenNotInViewport={true}>
+            <img
+              src={PayoutsBackgroundImage}
+              className="IndexPage__payouts__illustration__background"
+              alt="my payments tab in atlas studio"
+            />
+          </Plx>
+          <Plx parallaxData={parallaxDataForeground} animateWhenNotInViewport={true}>
+            <img
+              src={PayoutsForeground}
+              className="IndexPage__payouts__illustration__foreground"
+              alt="claim payout popup, in front of the my payments tab"
+            />
+          </Plx>
         </div>
       </div>
       <div className="IndexPage__payouts-cta">
