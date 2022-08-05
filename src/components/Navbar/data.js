91b03d979e67988449334366b38a7ef13d303d09
Edvin Dzidic <dzidic@student.tugraz.at>
2022-08-05 18:34:54 +0200

    Add primer to navbar

--- src/components/Navbar/data.js
+++ src/components/Navbar/data.js
@@ -5,6 +5,7 @@ const links = [
   { href: 'https://dao.joystream.org', label: 'navbar.pioneer' },
   { to: '/manifesto', label: 'navbar.manifesto' },
   { href: sharedData.social.discordLink, label: 'navbar.discord' },
+  { to: '/primer', label: 'navbar.primer' },
   {
     href: 'https://play.joystream.org/studio/',
     label: 'navbar.claimChannel',
