19b1959c5a1a22e240ceae1c33b01c86a982ff1e
Edvin Dzidic <edvindzidic2000@gmail.com>
2022-11-01 14:26:20 +0100

    Implement desktop 'Developers' and 'Development Steps' sections

--- src/components/index-page/Ecosystem/index.js
+++ src/components/index-page/Ecosystem/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-import { ReactComponent as InfoIcon } from '../../../assets/svg/landing/info.svg';
-import { ReactComponent as ArrowIcon } from '../../../assets/svg/arrow-down-small.svg';
 import GleevLogo from '../../../assets/images/landing/gleev-logo.png';
 import GleevIllustration from '../../../assets/images/landing/gleev-illustration.png';
 
+import { ReactComponent as InfoIcon } from '../../../assets/svg/landing/info.svg';
+import { ReactComponent as ArrowIcon } from '../../../assets/svg/arrow-down-small.svg';
+import { ReactComponent as CodeWindowControls } from '../../../assets/svg/landing/code-window-controls.svg';
+
 import './styles.scss';
+
+const DevelopmentStep = ({ stepNumber, sectionTitle, title, subtitle }) => (
+  <div className="IndexPage__ecosystem__developers__steps__item">
+    <p className="IndexPage__ecosystem__developers__steps__item__number">{stepNumber}</p>
+    <p className="IndexPage__ecosystem__developers__steps__item__section-title">{sectionTitle}</p>
+    <p className="IndexPage__ecosystem__developers__steps__item__title">{title}</p>
+    <p className="IndexPage__ecosystem__developers__steps__item__subtitle">{subtitle}</p>
+  </div>
+);
+
+const DEVELOPERS_SECTION_CODE = `
+`;
 
 const Ecosystem = () => {
   return (
@@ -53,7 +67,7 @@ const Ecosystem = () => {
             </div>
             <div className="IndexPage__ecosystem__apps__main__visual">
               <img src={GleevIllustration} alt="illustration of the gleev platform homepage" />
-              <div className='IndexPage__ecosystem__apps__main__visual__bottom-gradient'></div>
+              <div className="IndexPage__ecosystem__apps__main__visual__bottom-gradient"></div>
             </div>
           </div>
           <p className="IndexPage__ecosystem__apps__info">
@@ -61,6 +75,78 @@ const Ecosystem = () => {
             We've just launched, so our app collection is yet to grow. Check back soon for more exciting apps built on
             Joystream.
           </p>
+        </div>
+
+        <div className="IndexPage__ecosystem__developers">
+          <div className="IndexPage__ecosystem__developers__main">
+            <div className="IndexPage__ecosystem__developers__main__about">
+              <div className="IndexPage__ecosystem__developers__main__about__section-title">DEVELOPERS</div>
+              <h3 className="IndexPage__ecosystem__developers__main__about__title">
+                How about starting your own video community?
+              </h3>
+              <p className="IndexPage__ecosystem__developers__main__about__subtitle">
+                Spinning up your very own app was never easier before. Configurable feature set, no-code approach, and
+                support from the DAO lowers the set up barriers and lets you focus on your mission instead.
+              </p>
+              <a href="https://github.com/Joystream/atlas" target="_blank">
+                <div className="IndexPage__ecosystem__developers__main__about__link">
+                  Start your community{' '}
+                  <ArrowIcon className="IndexPage__ecosystem__developers__main__about__link__icon" />
+                </div>
+              </a>
+            </div>
+            <div className="IndexPage__ecosystem__developers__main__visual">
+              <CodeWindowControls />
+              {/* <div className='IndexPage__ecosystem__developers__main__visual__code'> */}
+              <pre className="IndexPage__ecosystem__developers__main__visual__code">
+                <span>1</span>
+                <code>git clone https://github.com/Joystream/atlas</code>
+                {'\n'}
+                <span>2</span>
+                <code>cd atlas</code>
+                {'\n'}
+                <span>3</span>
+                <code>yarn install</code>
+                {'\n'}
+                <span>4</span>
+                <code>yarn atlas:dev</code>
+                {'\n'}
+                <span>5</span>
+                {'\n'}
+                <span>6</span>
+                <code>// this is where you're stepping in <span role="img" aria-label='rocketship emoji'>🚀</span></code>
+                {'\n'}
+              </pre>
+              {/* </div> */}
+            </div>
+          </div>
+
+          <div className="IndexPage__ecosystem__developers__steps">
+            <DevelopmentStep
+              stepNumber="1"
+              sectionTitle="STEP 1"
+              title="Customize"
+              subtitle="Pick a name, logo & content theme. Update the design and make it yours."
+            />
+            <DevelopmentStep
+              stepNumber="2"
+              sectionTitle="STEP 2"
+              title="Launch"
+              subtitle="Make your webapp available to the world on your domain."
+            />
+            <DevelopmentStep
+              stepNumber="3"
+              sectionTitle="STEP 3"
+              title="Build"
+              subtitle="Build a community of creators and consumers around your business model."
+            />
+            <DevelopmentStep
+              stepNumber="4"
+              sectionTitle="STEP 4"
+              title="Earn"
+              subtitle="Get paid a share of the value the creators and audience generates."
+            />
+          </div>
         </div>
       </div>
     </section>
