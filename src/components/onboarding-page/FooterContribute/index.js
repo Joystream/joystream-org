012900500e1aa395188e84ba0c9702eea8707d0c
Edvin Dzidic <edvindzidic2000@gmail.com>
2022-11-14 23:56:56 +0100

    Remove start here page and related code

--- src/components/onboarding-page/FooterContribute/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './style.scss';
-
-const FooterContribute = ({ t }) => {
-  return <footer className="FooterContribute"></footer>;
-};
-
-export default FooterContribute;
