eeb38582926276a4b11c782245b6bd0da0dee35e
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-02-08 13:06:56 +0100

    Token page initial commit

--- src/components/token-page/Hero/index.js
+++ src/components/token-page/Hero/index.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { useTranslation, useI18next, Trans } from 'gatsby-plugin-react-i18next';
 
-import ArrowLink from '../../../components/ArrowLink';
-
-import Tokens from '../../../assets/svg/coin-minter-hero.svg';
-import TokensAlt from '../../../assets/svg/coin-minter-hero-alt.svg';
+import Tokens from '../../../assets/svg/token/joy-token-hero.svg';
+import TokensAlt from '../../../assets/svg/token/joy-token-hero-background.svg';
 
 import parseBalance from '../../../utils/parseBalance';
 
 import './style.scss';
 
 const TokenStatsItem = ({ title, value }) => (
-  <div className="TokensPage__tokenstats__item">
-    <h3 className="TokensPage__tokenstats__item__title">{title}</h3>
-    <p className="TokensPage__tokenstats__item__value">{value}</p>
+  <div className="TokenPage__tokenstats__item">
+    <h3 className="TokenPage__tokenstats__item__title">{title}</h3>
+    <p className="TokenPage__tokenstats__item__value">{value}</p>
   </div>
 );
 
 const TokenStatsItemPlaceholder = () => (
-  <div className="TokensPage__tokenstats__item TokensPage__tokenstats__item--placeholder">
-    <div className="TokensPage__tokenstats__item__title-placeholder"></div>
-    <div className="TokensPage__tokenstats__item__value-placeholder"></div>
+  <div className="TokenPage__tokenstats__item TokenPage__tokenstats__item--placeholder">
+    <div className="TokenPage__tokenstats__item__title-placeholder"></div>
+    <div className="TokenPage__tokenstats__item__value-placeholder"></div>
   </div>
 );
 
@@ -28,23 +26,24 @@ const TokenHero = ({ statusServerData }) => {
   const { t } = useTranslation();
 
   return (
-    <div className="TokensPage__header">
-      <div className="TokensPage__hero">
-        <div className="TokensPage__hero__content">
-          <h2 className="TokensPage__hero__title">{t('token.hero.title')}</h2>
-          <p className="TokensPage__hero__text">{t('token.hero.text')}</p>
-          <ArrowLink
-            className="TokensPage__hero__link"
-            to="/start-here/what-is-joystream"
-            text={t('button.gettingStarted')}
-          />
+    <div className="TokenPage__hero-background">
+      <div className="TokenPage__hero-wrapper">
+        <div className="TokenPage__hero">
+          <div className="TokenPage__hero__content">
+            <h2 className="TokenPage__hero__title">The JOY token</h2>
+            <p className="TokenPage__hero__text">
+              $JOY, being Joystream’s native token, is used to govern and keep the network secured. It’s used as a
+              currency in all dApps built on Joystream.
+            </p>
+          </div>
+          <div className="TokenPage__hero__illustration">
+            <img className="TokenPage__hero__illustration__image" alt="" src={Tokens} />
+            {/* <img className="TokenPage__hero__image TokenPage__hero__image--alt" alt="" src={TokensAlt} /> */}
+          </div>
         </div>
-        <img className="TokensPage__hero__image" alt="" src={Tokens} />
-        <img className="TokensPage__hero__image TokensPage__hero__image--alt" alt="" src={TokensAlt} />
-      </div>
-      <div className="TokensPage__tokenstats-wrapper">
-        <h2 className="TokensPage__tokenstats-title">{t('token.hero.tokenStats.title')}</h2>
-        <div className="TokensPage__tokenstats">
+        {/* <div className="TokenPage__tokenstats-wrapper">
+        <h2 className="TokenPage__tokenstats-title">{t('token.hero.tokenStats.title')}</h2>
+        <div className="TokenPage__tokenstats">
           {statusServerData ? (
             <>
               <TokenStatsItem
@@ -68,6 +67,7 @@ const TokenHero = ({ statusServerData }) => {
             </>
           )}
         </div>
+      </div> */}
       </div>
     </div>
   );
