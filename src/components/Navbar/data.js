d9a2c8c0682958c65b5660578f5a8279b1d666b5
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-04-27 15:49:55 +0200

    Revert lightpaper pdf name

--- src/components/Navbar/data.js
+++ src/components/Navbar/data.js
@@ -2,7 +2,7 @@ import { sharedData } from '../../data/pages';
 
 const links = [
   // Language "en" is a workaround to disallow gatsby to route to language specific routes here (e.g. ru/lightpaper.pdf)
-  { to: '/lightpaper_v1.2.pdf', label: 'navbar.lightpaper', language: 'en' },
+  { to: '/lightpaper.pdf', label: 'navbar.lightpaper', language: 'en' },
   { to: '/about', label: 'navbar.about' },
   { to: '/community', label: 'navbar.community' },
   { to: '/token', label: 'navbar.token' },
