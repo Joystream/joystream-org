ffef01cbf843ca6dbed479205d0bb98c2e166869
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-06-25 12:53:32 +0200

    Add MEXC to exchanges section on token page

--- src/components/token-page/Exchanges/index.js
+++ src/components/token-page/Exchanges/index.js
@@ -1,45 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import './style.scss'
+import ArrowLink from '../../ArrowLink';
+
+import MEXC from '../../../assets/images/token/mexc-logo.webp';
+
+import './style.scss';
+
+const ExchangeSection = ({ exchangeIcon, exchangeName, exchangeDescription, exchangeLink, t }) => {
+  return (
+    <div className="TokenPage__exchanges__item">
+      <div className="TokenPage__exchanges__item__image-wrapper">
+        <img src={exchangeIcon} className="TokenPage__exchanges__item__image" alt="" />
+      </div>
+      <p className="TokenPage__exchanges__item__name">{exchangeName}</p>
+      <p className="TokenPage__exchanges__item__description">{exchangeDescription}</p>
+      <ArrowLink href={exchangeLink} className="TokenPage__exchanges__item__link" text={t('token.exchanges.visit')} />
+    </div>
+  );
+};
 
 const Exchanges = ({ t }) => {
-  const [email, setEmail] = useState("");
-
   return (
     <section className="TokenPage__exchanges-wrapper">
-      <div className='TokenPage__exchanges'>
+      <div className="TokenPage__exchanges">
         <header className="TokenPage__exchanges__header">
-          <span className="TokenPage__exchanges__header__section-title">{t("token.exchanges.sectionTitle")}</span>
-          <h2 className="TokenPage__exchanges__header__title">{t("token.exchanges.title")}</h2>
-          <p className="TokenPage__exchanges__header__subtitle">
-            {t("token.exchanges.subtitle")}
-          </p>
+          <span className="TokenPage__exchanges__header__section-title">{t('token.exchanges.sectionTitle')}</span>
+          <h2 className="TokenPage__exchanges__header__title">{t('token.exchanges.title')}</h2>
+          <p className="TokenPage__exchanges__header__subtitle">{t('token.exchanges.subtitle')}</p>
         </header>
 
-        <div className='TokenPage__exchanges__newsletter'>
-          <h3 className='TokenPage__exchanges__newsletter__title'>{t("token.exchanges.newsletter.title")}</h3>
-          <p className='TokenPage__exchanges__newsletter__subtitle'>{t("token.exchanges.newsletter.subtitle")}</p>
-          <form
-            method="post"
-            action="https://joystream.us11.list-manage.com/subscribe/post?u=932de577aec9616d4516b4e0f&amp;id=459ba8d1da"
-            name="mc-embedded-subscribe-form"
-            target="_blank"
-            noValidate
-            className="TokenPage__exchanges__newsletter__form"
-          >
-            <input
-              className="TokenPage__exchanges__newsletter__form__input"
-              placeholder={t("token.exchanges.newsletter.placeholder")}
-              type="email"
-              name="EMAIL"
-              required
-              onChange={e => setEmail(e.target.value)}
-              value={email}
-            />
-            <button className="TokenPage__exchanges__newsletter__form__button" type="submit" name="subscribe">
-              {t("token.exchanges.newsletter.join")}
-            </button>
-          </form>
+        <div className="TokenPage__exchanges__items">
+          <ExchangeSection
+            exchangeIcon={MEXC}
+            exchangeName={t('token.exchanges.mexc.name')}
+            exchangeDescription={t('token.exchanges.mexc.description')}
+            exchangeLink="https://www.mexc.com/"
+            t={t}
+          />
         </div>
       </div>
     </section>
