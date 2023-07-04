f8c1175ca6fc89f5fc1d66a4df79e21292229880
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-07-04 15:25:07 +0200

    Implement image fallback for new asset array datatype (for nft and payouts carousels)

---
+++ src/utils/useImageFallback/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const useImageFallback = (imageList, placeholder) => {
+  const [imgData, setImgData] = useState({
+    src: imageList.length === 0 ? '' : imageList[0],
+    index: 0,
+  });
+
+  const onError = e => {
+    console.log('Error happened!');
+
+    if (imageList.length === 0 || imgData.index === imageList.length - 1) {
+      if (placeholder) {
+        e.currentTarget.src = placeholder;
+      }
+
+      return;
+    }
+
+    setImgData(prev => ({
+      src: imageList[prev.index + 1],
+      index: prev.index + 1,
+    }));
+  };
+
+  return [imgData.src, onError];
+};
+
+export default useImageFallback;
