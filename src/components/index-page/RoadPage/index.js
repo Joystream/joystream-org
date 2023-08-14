a2d75992e73b4191bd62ed7cf60eb3125ab33607
HeinrichOlfert <heinrich.olf@gmail.com>
2023-08-15 00:48:34 +0800

    road page start

---
+++ src/components/index-page/RoadPage/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+import { TokenStatsSection } from '../../token-page/Hero/index.js';
+
+import './style.scss';
+
+const RoadPages = ({ t }) => {
+  return (
+    <section className="IndexPage__tokenomics-wrapper">
+      <div className="IndexPage__tokenomics">
+        <header>
+          <span className="IndexPage__tokenomics__section-title">{t('landing.tokenomics.sectionTitle')}</span>
+          <h2 className="IndexPage__tokenomics__title">
+            <Trans i18nKey="landing.tokenomics.title" components={{ br: <br /> }} />
+          </h2>
+        </header>
+        <TokenStatsSection t={t} isSeparate={true} />
+      </div>
+    </section>
+  );
+};
+
+export default RoadPages;
