a52ab2a5f56fe131852eb9ab8cf2556c404f8549
HeinrichOlfert <heinrich.olf@gmail.com>
2023-08-15 00:50:24 +0800

    add language

--- src/components/index-page/RoadPage/index.js
+++ src/components/index-page/RoadPage/index.js
@@ -6,6 +6,22 @@ import { TokenStatsSection } from '../../token-page/Hero/index.js';
 import './style.scss';
 
 const RoadPages = ({ t }) => {
+
+
+//     fetch('https://api.github.com/repos/:owner/:repo', {
+//   headers: {
+//     'Authorization': 'Bearer YOUR_ACCESS_TOKEN'
+//     }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//     // Do something with the repository data
+//     })
+//     .catch(error => {
+//     // Handle any errors
+//     });
+
+
   return (
     <section className="IndexPage__tokenomics-wrapper">
       <div className="IndexPage__tokenomics">
