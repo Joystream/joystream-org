d5b0836eb64288e9202a06bfe52b370c67905c64
HeinrichOlfert <heinrich.olf@gmail.com>
2023-09-01 12:09:26 +0200

    change token

--- gitconfig.js
+++ gitconfig.js
@@ -3,4 +3,4 @@ export const GIT_REPOSITY = "Joystream_term_json_data";
 export const GIT_FOLDER = "goals";
 export const GIT_GLOSSARY_FOLDER = "glossary";
 export const ACCESS_TOKEN =
-  "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";
+  "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";
