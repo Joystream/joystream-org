1402970e882aa654b8e2c9861d5513ceb63de1d8
HeinrichOlfert <heinrich.olf@gmail.com>
2023-09-01 11:33:31 +0200

    update tooltip

--- gitconfig.js
+++ gitconfig.js
@@ -3,4 +3,4 @@ export const GIT_REPOSITY = "Joystream_term_json_data";
 export const GIT_FOLDER = "goals";
 export const GIT_GLOSSARY_FOLDER = "glossary";
 export const ACCESS_TOKEN =
-  "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";
+  "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";
