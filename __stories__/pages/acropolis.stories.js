8160f803e5e2738b0026812421ad782bfa35c82c
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-09-11 13:34:56 +0100

    Remove stories associated with removed pages

--- __stories__/pages/acropolis.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import AcropolisPage from '../../src/pages/acropolis';
-
-storiesOf('Pages|Acropolis', module).add('default', () => <AcropolisPage />);
--- __stories__/pages/athens.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import AthensPage from '../../src/pages/athens';
-
-storiesOf('Pages|Athens', module).add('default', () => <AthensPage />);
