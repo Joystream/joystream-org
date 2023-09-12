1e77e4286fb291d46b9721437394fb583729fff0
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-09-12 12:40:58 +0100

    Finalize desktop and mobile variants of press section

--- src/components/about-page/Press/index.js
+++ src/components/about-page/Press/index.js
@@ -9,36 +9,35 @@ import CoindeskLogo from '../../../assets/images/coindesk-logo.png';
 import './style.scss';
 
 const PressStory = ({ t }) => (
-  <div className="AboutPage__press__story">
-    <div className="AboutPage__press__story__about">
-      <div className="AboutPage__press__story__about__section-title">FEATURED ON</div>
-      <div className="AboutPage__press__story__about__logo-wrapper">
-        <img className="AboutPage__press__story__about__logo" src={CoindeskLogo} alt="coindesk logo" />
+  <a
+    href="ttps://www.coindesk.com/business/2022/03/23/decentralized-creator-platform-joystream-raises-58m/"
+    target="_blank"
+  >
+    <div className="AboutPage__press__story">
+      <div className="AboutPage__press__story__about">
+        <div className="AboutPage__press__story__about__section-title">FEATURED ON</div>
+        <div className="AboutPage__press__story__about__logo-wrapper">
+          <img className="AboutPage__press__story__about__logo" src={CoindeskLogo} alt="coindesk logo" />
+        </div>
+        <h4 className="AboutPage__press__story__about__title">Decentralized Creator Platform Joystream Raises $5.8M</h4>
+        <p className="AboutPage__press__story__about__platform-description">
+          The Polkadot-based platform allows creators to sell their videos as NFTs and turn their channels into tokens.
+        </p>
+        <p className="AboutPage__press__story__about__link">
+          Read article
+          <ArrowIcon className="AboutPage__press__story__about__link__icon" />
+        </p>
       </div>
-      <h4 className="AboutPage__press__story__about__platform-name">
-        Decentralized Creator Platform Joystream Raises $5.8M
-      </h4>
-      <p className="AboutPage__press__story__about__platform-description">
-        The Polkadot-based platform allows creators to sell their videos as NFTs and turn their channels into tokens.
-      </p>
-      <a
-        className="AboutPage__press__story__about__link"
-        href="https://www.coindesk.com/business/2022/03/23/decentralized-creator-platform-joystream-raises-58m/"
-        target="_blank"
-      >
-        Read article
-        <ArrowIcon className="AboutPage__press__story__about__link__icon" />
-      </a>
+      <div className="AboutPage__press__story__visual">
+        <img
+          className="AboutPage__press__story__visual__image"
+          src={CoindeskArticleScreenshot}
+          alt="coindesk article screenshot"
+        />
+        <div className="AboutPage__press__story__visual__bottom-gradient"></div>
+      </div>
     </div>
-    <div className="AboutPage__press__story__visual">
-      <img
-        className="AboutPage__press__story__visual__image"
-        src={CoindeskArticleScreenshot}
-        alt="coindesk article screenshot"
-      />
-      <div className="AboutPage__press__story__visual__bottom-gradient"></div>
-    </div>
-  </div>
+  </a>
 );
 
 const Press = () => {
