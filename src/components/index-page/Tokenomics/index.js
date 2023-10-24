f40f9d72b3ee069e06e41c5ef6e1d8cd1643a5f1
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-10-24 14:54:07 +0200

    Start initial work on Tokenomics section

--- src/components/index-page/Tokenomics/index.js
+++ src/components/index-page/Tokenomics/index.js
@@ -9,13 +9,27 @@ const Tokenomics = ({ t }) => {
   return (
     <section className="IndexPage__tokenomics-wrapper">
       <div className="IndexPage__tokenomics">
-        <header>
-          <span className="IndexPage__tokenomics__section-title">{t('landing.tokenomics.sectionTitle')}</span>
-          <h2 className="IndexPage__tokenomics__title">
-            <Trans i18nKey="landing.tokenomics.title" components={{ br: <br /> }} />
-          </h2>
+        <header className="IndexPage__tokenomics__header">
+          <span className="IndexPage__tokenomics__header__section-title">{t('landing.tokenomics.sectionTitle')}</span>
+          <h2 className="IndexPage__tokenomics__header__title">Token Metrics</h2>
         </header>
-        <TokenStatsSection t={t} isSeparate={true} />
+        <div className="IndexPage__tokenomics__metrics">
+          <div className="IndexPage__tokenomics__metrics__container IndexPage__tokenomics__metrics__container--price">
+            Price
+          </div>
+          <div className="IndexPage__tokenomics__metrics__container IndexPage__tokenomics__metrics__container--market-cap">
+            Market cap
+          </div>
+          <div className="IndexPage__tokenomics__metrics__container IndexPage__tokenomics__metrics__container--total-supply">
+            Total supply
+          </div>
+          <div className="IndexPage__tokenomics__metrics__container IndexPage__tokenomics__metrics__container--circulating-supply">
+            Circulating supply
+          </div>
+          <div className="IndexPage__tokenomics__metrics__container IndexPage__tokenomics__metrics__container--fdv">
+            FDV
+          </div>
+        </div>
       </div>
     </section>
   );
