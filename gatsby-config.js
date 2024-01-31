04f4c250820fbfbc2abaede54dd24f5e6b676d13
Edvin Dzidic <edvindzidic2000@gmail.com>
2024-01-31 11:27:28 +0100

    Add package that splits up css, optimize profiles-metric.svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,6 +80,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-split-css`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
