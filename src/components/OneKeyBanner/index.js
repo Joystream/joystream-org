7cb27f62f7535239380b5e83d3b7f422257f45ca
Edvin Dzidic <edvindzidic2000@gmail.com>
2024-03-22 16:37:15 +0100

    Implement persistent state to track whether banner should be open, finalize styling and add RWD

--- src/components/OneKeyBanner/index.js
+++ src/components/OneKeyBanner/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import OneKeyBackground from '../../assets/images/onekey.webp';
 import { ReactComponent as CloseIcon } from '../../assets/svg/postponed.svg';
 
 import './style.scss';
 
+const ONE_KEY_LOCAL_STORAGE_KEY = 'JOYSTREAM::showOneKeyBanner';
+
 const OneKeyBanner = () => {
+  const [shouldShow, setShouldShow] = React.useState(true);
+
+  useEffect(() => {
+    if (shouldShow === false) {
+      localStorage.setItem(ONE_KEY_LOCAL_STORAGE_KEY, false);
+    }
+  }, [shouldShow]);
+
+  if (localStorage.getItem(ONE_KEY_LOCAL_STORAGE_KEY) === 'false' || !shouldShow) return null;
+
   return (
     <div className="OneKeyBanner">
       <div className="OneKeyBanner__content">
@@ -13,10 +25,15 @@ const OneKeyBanner = () => {
         <div className="OneKeyBanner__content__subtitle">Learn more about limited edition OneKey Classic wallet</div>
       </div>
       <div className="OneKeyBanner__controls">
-        <a href="https://blog.joystream.org/limited-edition-onekey-x-joystream/">
+        <a href="https://blog.joystream.org/limited-edition-onekey-x-joystream/" target="_blank" rel="noreferrer">
           <div className="OneKeyBanner__controls__button">Learn More</div>
         </a>
-        <CloseIcon className="OneKeyBanner__controls__close" />
+        <CloseIcon
+          className="OneKeyBanner__controls__close"
+          onClick={() => {
+            setShouldShow(false);
+          }}
+        />
       </div>
       <img src={OneKeyBackground} alt="OneKey" className="OneKeyBanner__background" />
     </div>
