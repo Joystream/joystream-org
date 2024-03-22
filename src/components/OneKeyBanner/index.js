604c97c9e4d13d9f3d3def0495c6186d33498c96
Edvin Dzidic <edvindzidic2000@gmail.com>
2024-03-22 20:17:29 +0100

    Move UI into if statement and make null the default returned value

--- src/components/OneKeyBanner/index.js
+++ src/components/OneKeyBanner/index.js
@@ -14,29 +14,31 @@ const shouldBannerBeClosed = () => {
 const OneKeyBanner = () => {
   const [shouldShow, setShouldShow] = React.useState(!shouldBannerBeClosed() ? true : false);
 
-  if (shouldShow === false) return null;
+  if (shouldShow) {
+    return (
+      <div className="OneKeyBanner">
+        <div className="OneKeyBanner__content">
+          <div className="OneKeyBanner__content__title">Joystream Hardware Wallet Sale is Now Live!</div>
+          <div className="OneKeyBanner__content__subtitle">Learn more about limited edition OneKey Classic wallet</div>
+        </div>
+        <div className="OneKeyBanner__controls">
+          <a href="https://blog.joystream.org/limited-edition-onekey-x-joystream/" target="_blank" rel="noreferrer">
+            <div className="OneKeyBanner__controls__button">Learn More</div>
+          </a>
+          <CloseIcon
+            className="OneKeyBanner__controls__close"
+            onClick={() => {
+              setShouldShow(false);
+              typeof window !== 'undefined' && window.localStorage.setItem(ONE_KEY_LOCAL_STORAGE_KEY, 'false');
+            }}
+          />
+        </div>
+        <img src={OneKeyBackground} alt="OneKey" className="OneKeyBanner__background" />
+      </div>
+    );
+  }
 
-  return (
-    <div className="OneKeyBanner">
-      <div className="OneKeyBanner__content">
-        <div className="OneKeyBanner__content__title">Joystream Hardware Wallet Sale is Now Live!</div>
-        <div className="OneKeyBanner__content__subtitle">Learn more about limited edition OneKey Classic wallet</div>
-      </div>
-      <div className="OneKeyBanner__controls">
-        <a href="https://blog.joystream.org/limited-edition-onekey-x-joystream/" target="_blank" rel="noreferrer">
-          <div className="OneKeyBanner__controls__button">Learn More</div>
-        </a>
-        <CloseIcon
-          className="OneKeyBanner__controls__close"
-          onClick={() => {
-            setShouldShow(false);
-            typeof window !== 'undefined' && window.localStorage.setItem(ONE_KEY_LOCAL_STORAGE_KEY, 'false');
-          }}
-        />
-      </div>
-      <img src={OneKeyBackground} alt="OneKey" className="OneKeyBanner__background" />
-    </div>
-  );
+  return null;
 };
 
 export default OneKeyBanner;
