a09d772f560b1b4c83455079aae0d93dde7084ce
Edvin Dzidic <edvindzidic2000@gmail.com>
2024-07-18 13:35:53 +0200

    Update old ones and add new verified members

--- src/data/pages/verification.js
+++ src/data/pages/verification.js
@@ -28,58 +28,6 @@ export const verifiedMembers = [
     },
   },
   {
-    memberHandle: 'zhsojib2020',
-    avatarUrl: 'https://atlas-services.joystream.org/avatars/384efba7-8c21-4262-bc9d-105672a83f4e.webp',
-    title: 'verification.title.outreachSpecialist',
-    socials: {
-      telegram: '@zhsojib',
-      twitter: '@zhsojib2021',
-      email: 'airdropfinderbd@gmail.com',
-      discord: '@zhsojib2020',
-    },
-    safety: {
-      notAllowed: [
-        { text: 'verification.safety.willNever.items.askForMoney' },
-        { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
-        { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
-        {
-          text: 'verification.safety.willNever.items.askToVisitLinks',
-          components: [{ link: 'https://www.joystream.org', text: 'Joystream.org' }],
-        },
-      ],
-      allowed: [
-        { text: 'verification.safety.can.items.inviteToYpp' },
-        { text: 'verification.safety.can.items.offerMarketingSupport' },
-      ],
-    },
-  },
-  {
-    memberHandle: 'spat_sochi',
-    avatarUrl: 'https://raw.githubusercontent.com/Joystream/founding-members/main/avatars/primary-avatar/59.png',
-    title: 'verification.title.communitySupport',
-    socials: {
-      telegram: '@spat_sochi',
-      twitter: '@spat_sochi',
-      email: 'spatsochi85@gmail.com',
-      discord: '@spat_sochi',
-    },
-    safety: {
-      notAllowed: [
-        { text: 'verification.safety.willNever.items.askForMoney' },
-        { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
-        { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
-        {
-          text: 'verification.safety.willNever.items.askToVisitLinks',
-          components: [{ link: 'https://www.joystream.org', text: 'Joystream.org' }],
-        },
-      ],
-      allowed: [
-        { text: 'verification.safety.can.items.answerJoystreamRelatedInquiries' },
-        { text: 'verification.safety.can.items.promoteGovernance' },
-      ],
-    },
-  },
-  {
     memberHandle: 'Genius',
     avatarUrl: 'https://raw.githubusercontent.com/Joystream/founding-members/main/avatars/primary-avatar/203.png',
     title: 'verification.title.communitySupport',
@@ -132,34 +80,6 @@ export const verifiedMembers = [
     },
   },
   {
-    memberHandle: 'tomato',
-    avatarUrl: 'https://raw.githubusercontent.com/Joystream/founding-members/main/avatars/primary-avatar/31.png',
-    title: 'verification.title.membershipLead',
-    socials: {
-      telegram: '@tomato_js',
-      twitter: '@abcxyz_tomato',
-      email: 'abc@doctrinemachine.xyz',
-      discord: '@tomato_js',
-    },
-    safety: {
-      notAllowed: [
-        { text: 'verification.safety.willNever.items.askForMoney' },
-        { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
-        { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
-        {
-          text: 'verification.safety.willNever.items.askToVisitLinks',
-          components: [{ link: 'https://www.joystream.org', text: 'Joystream.org' }],
-        },
-      ],
-      allowed: [
-        { text: 'verification.safety.can.items.helpWithMembershipCreation' },
-        { text: 'verification.safety.can.items.verifyChannels' },
-        { text: 'verification.safety.can.items.verifyValidators' },
-        { text: 'verification.safety.can.items.askToVerifyOwnership' },
-      ],
-    },
-  },
-  {
     memberHandle: '0x2bc',
     avatarUrl: 'https://raw.githubusercontent.com/Joystream/founding-members/main/avatars/primary-avatar/28.png',
     title: 'verification.title.distributionAndStorageWorker',
@@ -173,12 +93,9 @@ export const verifiedMembers = [
         { text: 'verification.safety.willNever.items.askForMoney' },
         { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
         { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
-        {
-          text: 'verification.safety.willNever.items.askToVisitLinks',
-          components: [{ link: 'https://www.joystream.org', text: 'Joystream.org' }],
-        },
+        { text: 'verification.safety.willNever.items.askToVisitUnrelatedLinks' },
       ],
-      allowed: [{ text: 'verification.safety.can.items.assistWithStorageAndDistribution' }],
+      allowed: [{ text: 'verification.safety.can.items.assistWithStorage' }],
     },
   },
   {
@@ -238,14 +155,58 @@ export const verifiedMembers = [
         { text: 'verification.safety.willNever.items.askForMoney' },
         { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
         { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
-        {
-          text: 'verification.safety.willNever.items.askToVisitLinks',
-          components: [{ link: 'https://www.joystream.org', text: 'Joystream.org' }],
-        },
+        { text: 'verification.safety.willNever.items.askToVisitUnrelatedLinks' },
       ],
       allowed: [
         { text: 'verification.safety.can.items.assistWithBuilders' },
         { text: 'verification.safety.can.items.assistWithApps' },
+      ],
+    },
+  },
+  {
+    memberHandle: 'vikan#4315',
+    substituteUserRoute: 'vikan',
+    avatarUrl: 'https://raw.githubusercontent.com/Joystream/founding-members/main/avatars/primary-avatar/90.png',
+    title: 'verification.title.communitySupport',
+    socials: {
+      telegram: '@Vikan393',
+      twitter: '@jVikan1',
+      email: 'Obiechinaodo@gmail.com',
+      discord: '@v.i.k.a.n',
+    },
+    safety: {
+      notAllowed: [
+        { text: 'verification.safety.willNever.items.askForMoney' },
+        { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
+        { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
+        { text: 'verification.safety.willNever.items.askToVisitUnrelatedLinks' },
+      ],
+      allowed: [
+        { text: 'verification.safety.can.items.answerJoystreamRelatedInquiries' },
+        { text: 'verification.safety.can.items.promoteGovernance' },
+      ],
+    },
+  },
+  {
+    memberHandle: 'zhsojib2020',
+    avatarUrl: 'https://atlas-services.joystream.org/avatars/384efba7-8c21-4262-bc9d-105672a83f4e.webp',
+    title: 'verification.title.communitySupport',
+    socials: {
+      telegram: '@zhsojib',
+      twitter: '@zhsojib2021',
+      email: 'airdropfinderbd@gmail.com',
+      discord: '@zhsojib2020',
+    },
+    safety: {
+      notAllowed: [
+        { text: 'verification.safety.willNever.items.askForMoney' },
+        { text: 'verification.safety.willNever.items.askForPasswordsOrSensitiveInformation' },
+        { text: 'verification.safety.willNever.items.sendAnythingDangerous' },
+        { text: 'verification.safety.willNever.items.askToVisitUnrelatedLinks' },
+      ],
+      allowed: [
+        { text: 'verification.safety.can.items.answerJoystreamRelatedInquiries' },
+        { text: 'verification.safety.can.items.promoteGovernance' },
       ],
     },
   },
